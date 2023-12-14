e1d6ec873e92cba8a9c56ea0e14f652c5f6fb9b2
Miguel Medeiros <miguel@miguelmedeiros.com.br>
2023-12-14 10:04:35 -0300

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Social UI Monorepo
+
+## Start the app
+
+To start the development server run `nx serve web`.
+Open your browser and navigate to http://localhost:4200/. Happy coding!
---
+++ apps/web/app/page.tsx
@@ -0,0 +1,16 @@
+export default async function Index() {
+  return (
+    <div className="container">
+      <div className="wrapper">
+        <div className="container">
+          <div id="welcome">
+            <h1>
+              <span> Hello there, </span>
+              Welcome web app👋
+            </h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
