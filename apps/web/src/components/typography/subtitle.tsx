dee57f73e02837f438c6513b11bc1b977b6cc4eb
flaviomoceri <moceri.flavio@gmail.com>
2024-01-11 15:15:32 +0100

    Feat (onboarding): add Subtitle component

---
+++ apps/web/src/components/typography/subtitle.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+interface OnBoardingSubTitleProps {
+  color?: string;
+  children: string;
+  styles?: string;
+}
+
+export const OnBoardingSubTitle = ({
+  color = 'text-white',
+  children,
+  styles,
+  ...props
+}: OnBoardingSubTitleProps) => {
+  const textColor = `${color}`;
+
+  return (
+    <h2
+      className={`font-inter-tight text-[30px] font-light leading-[30px] mt-6 mb-12 tracking-[-0.3px] text-opacity-50 ${textColor} ${styles}`}
+      {...props}
+    >
+      {children}
+    </h2>
+  );
+};
