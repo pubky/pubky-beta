f3c0086dd948668387daf44f0703d6d5333670e5
flaviomoceri <moceri.flavio@gmail.com>
2024-01-11 15:26:51 +0100

    Feat: add Button component

---
+++ apps/web/src/components/buttons/index.tsx
@@ -0,0 +1,113 @@
+import React from 'react';
+
+interface PrimaryButtonProps {
+  title: string;
+  svg?: React.ReactNode;
+  state?: string;
+  width?: string;
+}
+
+interface SecondaryButtonProps {
+  title: string;
+  svg?: React.ReactNode;
+  state?: string;
+  width?: string;
+}
+
+export const PrimaryButton = ({
+  title,
+  svg,
+  state = 'default',
+  width = '100%',
+}: PrimaryButtonProps) => {
+  function getGradientValue(state: string) {
+    switch (state) {
+      case 'active':
+        return ['0.5', '0.5', '0.5', '0.8'];
+      case 'disable':
+        return ['0.1', '0.1', '0.1', '0.3'];
+      default:
+        return ['0.2', '0.2', '0.2', '0.6'];
+    }
+  }
+
+  const [gradient1, gradient2, gradient3, gradient4] = getGradientValue(state);
+
+  const buttonStyle = {
+    width: width,
+    height: '56px',
+    padding: '20px 24px',
+    borderRadius: '64px',
+    border: '2px solid',
+    gap: '40px',
+    background: `
+      conic-gradient(from -45deg at 50% 50%, rgba(253, 0, 255, ${gradient1}) 0deg, rgba(0, 255, 93, ${gradient2}) 118.12deg, rgba(0, 75, 255, ${gradient3}) 238.12deg, rgba(253, 0, 255, ${gradient1}) 360deg),
+      conic-gradient(from -45deg at 50% 50%, rgba(253, 0, 255, ${gradient1}) 0deg, rgba(0, 255, 93, ${gradient2}) 118.12deg, rgba(0, 75, 255, ${gradient3}) 238.12deg, rgba(253, 0, 255, ${gradient1}) 360deg)
+    `,
+    boxShadow: '0px 25px 50px 0px rgba(0, 0, 0, 0.25)',
+    borderImage: `conic-gradient(from -45deg at 50% 50%, 
+        rgba(253, 0, 255, ${gradient4}) 0deg, 
+        rgba(0, 255, 93, ${gradient4}) 118.12deg, 
+        rgba(0, 75, 255, ${gradient4}) 238.12deg, 
+        rgba(253, 0, 255, ${gradient4}) 360deg
+      )`,
+    borderImageSlice: '1',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: 'white',
+    fontWeight: 'bold',
+    cursor: 'pointer',
+    marginTop: '24px',
+  };
+
+  return (
+    <button style={buttonStyle}>
+      <p className="flex center font-semibold text-[15px] leading-[18px] tracking-[0.2px]">
+        {svg}
+        <span className="ml-[6px]">{title}</span>
+      </p>
+    </button>
+  );
+};
+
+export const SecondaryButton = ({
+  title,
+  svg,
+  state = 'default',
+  width = '100%',
+}: SecondaryButtonProps) => {
+  function getGradientValue(state: string) {
+    switch (state) {
+      case 'active':
+        return ['0.32'];
+      default:
+        return ['0.16'];
+    }
+  }
+
+  const [gradient] = getGradientValue(state);
+
+  const buttonStyle = {
+    width: width,
+    padding: '20px 24px',
+    borderRadius: '64px',
+    gap: '40px',
+    backgroundColor: `rgba(255, 255, 255, ${gradient})`,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: 'white',
+    fontWeight: 'bold',
+    cursor: 'pointer',
+  };
+
+  return (
+    <button style={buttonStyle}>
+      <p className="flex center font-semibold text-[15px] leading-[18px] tracking-[0.2px]">
+        {svg}
+        <span className="ml-[6px]">{title}</span>
+      </p>
+    </button>
+  );
+};
