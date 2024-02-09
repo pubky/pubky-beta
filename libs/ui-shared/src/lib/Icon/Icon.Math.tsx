6d8f3be3745e2f3225d8c6e294d91693f3286be6
flaviomoceri <moceri.flavio@gmail.com>
2024-02-09 11:04:03 +0100

    fix: fix svg fill-clip/rule

--- libs/ui-shared/src/lib/Icon/Icon.Math.tsx
+++ libs/ui-shared/src/lib/Icon/Icon.Math.tsx
@@ -12,14 +12,14 @@ export const Plus = ({ size = '16', color = 'white' }: IconProps) => (
     xmlns="http://www.w3.org/2000/svg"
   >
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M2 8C2 7.72386 2.22386 7.5 2.5 7.5H13.5C13.7761 7.5 14 7.72386 14 8C14 8.27614 13.7761 8.5 13.5 8.5H2.5C2.22386 8.5 2 8.27614 2 8Z"
       fill={color}
     />
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M8 2C8.27614 2 8.5 2.22386 8.5 2.5V13.5C8.5 13.7761 8.27614 14 8 14C7.72386 14 7.5 13.7761 7.5 13.5V2.5C7.5 2.22386 7.72386 2 8 2Z"
       fill={color}
     />
@@ -35,8 +35,8 @@ export const Minus = ({ size = '16', color = 'white' }: IconProps) => (
     xmlns="http://www.w3.org/2000/svg"
   >
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M2 8C2 7.72386 2.22386 7.5 2.5 7.5L13.5 7.5C13.7761 7.5 14 7.72386 14 8C14 8.27614 13.7761 8.5 13.5 8.5H2.5C2.22386 8.5 2 8.27614 2 8Z"
       fill={color}
     />
