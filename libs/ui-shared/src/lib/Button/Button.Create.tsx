19f2c472a49b9502611c9b10b2454da6b8587680
Miguel Medeiros <miguel@miguelmedeiros.com.br>
2024-02-20 14:11:15 -0300

    refactor: button create

--- libs/ui-shared/src/lib/Button/Button.Create.tsx
+++ libs/ui-shared/src/lib/Button/Button.Create.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { twMerge } from 'tailwind-merge';
 import { Icon } from '../Icon';
 
 interface CreateButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
@@ -6,22 +7,19 @@ interface CreateButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
   height?: string;
   icon?: React.ReactNode;
   styles?: string;
-  className?: string;
 }
 
 export const Create = ({
-  width = 'w-[96px]',
-  height = 'h-[96px]',
   icon = <Icon.Pencil />,
-  styles = '',
   ...rest
 }: CreateButtonProps) => {
-  const iconStyle =
-    'hover:transition-[transform] hover:duration-[0.3s] hover:ease-[ease] hover:rotate-[20deg]';
-  const cssStyle = `${width} ${height} ${iconStyle} border-[11px]  border-fuchsia-500 hover:bg-fuchsia-500 hover:bg-opacity-30 rounded-[96px] flex items-center justify-center cursor-pointer`;
-
   return (
-    <button className={`${cssStyle} ${styles}`} {...rest}>
+    <button
+      className={twMerge(
+        `w-[96px] h-[96px] hover:transition-[transform] hover:duration-[0.3s] hover:ease-[ease] hover:rotate-[20deg] border-[11px] border-fuchsia-500 hover:bg-fuchsia-500 hover:bg-opacity-30 rounded-[96px] flex items-center justify-center cursor-pointer`,
+        rest.className
+      )}
+    >
       {icon}
     </button>
   );
