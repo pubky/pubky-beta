4a1bf9f534b2199b9502e00fb1d19b587f5bb780
Miguel Medeiros <miguel@miguelmedeiros.com.br>
2024-02-20 23:40:14 -0300

    refactor: Typography label

--- libs/ui-shared/src/lib/Typography/Typography.Label.tsx
+++ libs/ui-shared/src/lib/Typography/Typography.Label.tsx
@@ -1,20 +1,18 @@
+import { twMerge } from 'tailwind-merge';
+
 interface LabelProps extends React.HTMLAttributes<HTMLParagraphElement> {
-  color?: string;
   children: string;
-  styles?: string;
   id?: string;
 }
 
-export const Label = ({
-  color = 'text-white',
-  children,
-  styles = '',
-  ...rest
-}: LabelProps) => {
+export const Label = ({ children, ...rest }: LabelProps) => {
   return (
     <p
-      className={`text-[13px] font-semibold font-['Inter Tight'] uppercase tracking-wide ${color} ${styles}`}
       {...rest}
+      className={twMerge(
+        `text-[13px] font-semibold font-['Inter Tight'] uppercase tracking-wide text-white`,
+        rest.className
+      )}
     >
       {children}
     </p>
