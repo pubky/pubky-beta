eae5112b555289256b58b6d50b0c031a42538083
Miguel Medeiros <miguel@miguelmedeiros.com.br>
2024-02-21 09:47:45 -0300

    feat: add radio input

--- libs/ui-shared/src/lib/Input/index.tsx
+++ libs/ui-shared/src/lib/Input/index.tsx
@@ -1,15 +1,17 @@
 import { Checkbox } from './Input.Checkbox';
 import { CheckboxIcon } from './Input.CheckboxIcon';
 import { Cursor } from './Input.Cursor';
+import { Dropdown } from './Input.Dropdown';
+import { Radio } from './Input.Radio';
 import { Text } from './Input.Text';
 import { TextArea } from './Input.TextArea';
-import { Dropdown } from './Input.Dropdown';
 
 export const Input = {
   Checkbox,
   CheckboxIcon,
   Cursor,
+  Dropdown,
+  Radio,
   Text,
   TextArea,
-  Dropdown,
 };
