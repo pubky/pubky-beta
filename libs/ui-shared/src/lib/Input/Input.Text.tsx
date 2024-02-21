5a86234b86aea9360d144a538b2a4b05ebe440c3
Miguel Medeiros <miguel@miguelmedeiros.com.br>
2024-02-20 23:40:47 -0300

    refactor: Input Text

--- libs/ui-shared/src/lib/Input/Input.Text.tsx
+++ libs/ui-shared/src/lib/Input/Input.Text.tsx
@@ -1,66 +1,46 @@
+import { twMerge } from 'tailwind-merge';
 import { Typography } from '../Typography';
 
 interface TextProps
   extends React.HTMLAttributes<HTMLInputElement | HTMLTextAreaElement> {
-  value?: string;
+  value: string;
   placeHolder?: string;
   label?: string;
-  width?: string;
-  height?: string;
-  padding?: string;
-  styles?: string;
   multiline?: boolean;
   className?: string;
-  icon?: React.ReactNode;
+  action?: React.ReactNode;
 }
 
 export const Text = ({
   value,
   placeHolder,
   label,
-  width = 'w-full',
-  height = 'h-[70px]',
-  padding = 'p-6',
-  multiline,
-  styles,
-  icon,
+  action,
   ...rest
 }: TextProps) => {
-  const cssStyle = `${width} ${height} ${padding} bg-white bg-opacity-10 rounded-lg shadow-[0_4px_8px_0_rgba(0,0,0,0.32)_inset] border border-white border-opacity-10 flex-col justify-start items-start inline-flex outline-none`;
-  const inputTextStyle = `text-white text-opacity-80 placeholder:text-white placeholder:text-opacity-30 text-[17px] font-normal font-['Inter Tight'] leading-snug tracking-wide`;
-
   return (
     <>
       {label && (
-        <Typography.Label
-          color="text-white text-opacity-30"
-          styles="uppercase mb-2"
-        >
+        <Typography.Label className="text-white text-opacity-30 mb-2">
           {label}
         </Typography.Label>
       )}
-      {multiline ? (
-        <textarea
-          className={`${cssStyle} ${inputTextStyle} resize-none ${styles} `}
+      <div className="relative w-full">
+        <input
+          {...rest}
+          className={twMerge(
+            `w-full h-[70px] p-6 bg-white bg-opacity-10 rounded-lg shadow-[0_4px_8px_0_rgba(0,0,0,0.32)_inset] border border-white border-opacity-10 flex-col justify-start items-start inline-flex outline-none text-white text-opacity-80 placeholder:text-white placeholder:text-opacity-30 text-[17px] font-normal font-['Inter Tight'] leading-snug tracking-wide`,
+            rest.className
+          )}
           placeholder={placeHolder}
           value={value}
-          {...rest}
         />
-      ) : (
-        <div className="relative w-full">
-          <input
-            className={`${cssStyle} ${inputTextStyle} ${styles}`}
-            placeholder={placeHolder}
-            value={value}
-            {...rest}
-          />
-          {icon && (
-            <div className="absolute top-1/2 right-4 transform -translate-y-1/2 text-white">
-              {icon}
-            </div>
-          )}
-        </div>
-      )}
+        {action && (
+          <div className="absolute top-1/2 right-4 transform -translate-y-1/2 text-white">
+            {action}
+          </div>
+        )}
+      </div>
     </>
   );
 };
