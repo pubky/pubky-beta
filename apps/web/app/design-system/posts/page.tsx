45f331a41b340ef487c7fc3b3c30637942534b36
flaviomoceri <moceri.flavio@gmail.com>
2024-02-21 16:09:23 +0100

    feat: add stat component

--- apps/web/app/design-system/posts/page.tsx
+++ apps/web/app/design-system/posts/page.tsx
@@ -4,6 +4,7 @@ export default function Index() {
   return (
     <div className="flex-1 w-full h-screen bg-black p-10">
       <Post.Counter counter={3} />
+      <Post.Stat label="Followers">423</Post.Stat>
     </div>
   );
 }
