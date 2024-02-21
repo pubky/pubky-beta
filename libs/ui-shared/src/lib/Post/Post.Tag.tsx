fdeed3e3e89b939737e6f789ae709a945bb0c414
Miguel Medeiros <miguel@miguelmedeiros.com.br>
2024-02-20 23:40:28 -0300

    refactor: Post Tag

--- libs/ui-shared/src/lib/Post/Post.Tag.tsx
+++ libs/ui-shared/src/lib/Post/Post.Tag.tsx
@@ -1,69 +1,68 @@
-'use client';
-
-import { useState } from 'react';
 import { Typography } from '../Typography';
+import { twMerge } from 'tailwind-merge';
 
 interface TagProps extends React.HTMLAttributes<HTMLButtonElement> {
-  children: string;
+  clicked: boolean;
   color?: string;
-  styles?: string;
-  className?: string;
+  children: string;
 }
 
-export const Tag = ({ styles, color, children, ...rest }: TagProps) => {
-  const [clicked, setClicked] = useState(false);
-
-  let backgroundColor = clicked
+export const Tag = ({
+  clicked = false,
+  color,
+  children,
+  ...rest
+}: TagProps) => {
+  let cssClasses = clicked
     ? 'bg-white bg-opacity-20 border-transparent border-white'
     : 'bg-white bg-opacity-20 border-transparent hover:bg-opacity-30';
 
   switch (color) {
     case 'yellow':
-      backgroundColor = clicked
+      cssClasses = clicked
         ? 'bg-yellow-400 bg-opacity-30 border-transparent border-yellow-400'
         : 'bg-yellow-400 bg-opacity-30 border-transparent hover:bg-opacity-60';
       break;
     case 'amber':
-      backgroundColor = clicked
+      cssClasses = clicked
         ? 'bg-amber-500 bg-opacity-30 border-amber-500'
         : 'bg-amber-500 bg-opacity-30 border-transparent hover:bg-opacity-60';
       break;
     case 'red':
-      backgroundColor = clicked
+      cssClasses = clicked
         ? 'bg-red-600 bg-opacity-30 border-red-600'
         : 'bg-red-600 bg-opacity-30 border-transparent hover:bg-opacity-60';
       break;
     case 'fuchsia':
-      backgroundColor = clicked
+      cssClasses = clicked
         ? 'bg-fuchsia-500 bg-opacity-30 border-fuchsia-500'
         : 'bg-fuchsia-500 bg-opacity-30 border-transparent hover:bg-opacity-60';
       break;
     case 'blue':
-      backgroundColor = clicked
+      cssClasses = clicked
         ? 'bg-blue-600 bg-opacity-30 border-blue-600'
         : 'bg-blue-600 bg-opacity-30 border-transparent hover:bg-opacity-60';
       break;
     case 'cyan':
-      backgroundColor = clicked
+      cssClasses = clicked
         ? 'bg-cyan-400 bg-opacity-30 border-cyan-400'
         : 'bg-cyan-400 bg-opacity-30 border-transparent hover:bg-opacity-60';
       break;
     case 'green':
-      backgroundColor = clicked
+      cssClasses = clicked
         ? 'bg-green-500 bg-opacity-30 border-green-500'
         : 'bg-green-500 bg-opacity-30 border-transparent hover:bg-opacity-60';
       break;
   }
 
-  const handleClick = () => {
-    setClicked(!clicked);
-  };
-
   return (
     <button
-      onClick={handleClick}
-      className={`border ${backgroundColor} h-8 px-3 py-1 rounded-lg cursor-pointer ${styles}`}
       {...rest}
+      className={twMerge(
+        `border h-8 px-3 py-1 rounded-lg cursor-pointer`,
+        cssClasses,
+        rest.className
+      )}
     >
       <Typography.Body variant="small-bold">{children}</Typography.Body>
     </button>
