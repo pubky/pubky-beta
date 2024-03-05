6c81a0fc6bc04d1ba2fb014663443fa84617bde6
flaviomoceri <moceri.flavio@gmail.com>
2024-03-05 08:54:50 +0100

    fix: change LinksStoreApp

--- libs/ui-shared/src/lib/Content/Content.LinksStoreApp.tsx
+++ libs/ui-shared/src/lib/Content/Content.LinksStoreApp.tsx
@@ -14,16 +14,34 @@ export const LinksStoreApp = ({ ...rest }: LinksStoreAppProps) => {
   const baseCSS = 'flex-col justify-start items-start gap-6 inline-flex';
   return (
     <div {...rest} className={twMerge(baseCSS, rest.className)}>
-      <Link href="https://testflight.apple.com/join/lGXhnwcC">
-        <Card.Primary className="w-full gap-2 md:flex-row lg:flex-col xl:flex-row xl:w-80">
+      <Link
+        className="w-full"
+        href="https://testflight.apple.com/join/lGXhnwcC"
+      >
+        <Card.Primary className="p-6 flex-row xl:p-8">
           <Image width={70} height={25} src={bitkit} alt="bitkit" />
-          <Image width={116} height={24} src={iphone} alt="iphone" />
+          <Image
+            className="scale-50 md:scale-75 lg:scale-50 xl:scale-100"
+            width={116}
+            height={24}
+            src={iphone}
+            alt="iphone"
+          />
         </Card.Primary>
       </Link>
-      <Link href="https://play.google.com/apps/testing/to.synonym.bitkit.wallet">
-        <Card.Primary className="w-full gap-2 md:flex-row lg:flex-col xl:flex-row xl:w-80">
+      <Link
+        className="w-full"
+        href="https://play.google.com/apps/testing/to.synonym.bitkit.wallet"
+      >
+        <Card.Primary className="p-6 flex-row xl:p-8">
           <Image width={70} height={25} src={bitkit} alt="bitkit" />
-          <Image width={157} height={24} src={android} alt="android" />
+          <Image
+            className="scale-50 md:scale-75 lg:scale-50 xl:scale-100"
+            width={157}
+            height={24}
+            src={android}
+            alt="android"
+          />
         </Card.Primary>
       </Link>
     </div>
