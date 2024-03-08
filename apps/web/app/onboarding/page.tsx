f9cb032e224c150b16e3b109e0e98aa2ab7ddebf
Flavio <moceri.flavio@gmail.com>
2024-03-08 16:04:14 +0100

    feat(onboarding): add bg images (#113)
    * feat(onboarding): add bg images
    
    * fix: removed unnecessary wrapper
    
    ---------
    
    Co-authored-by: Miguel Medeiros <miguel@miguelmedeiros.com.br>

--- apps/web/app/onboarding/page.tsx
+++ apps/web/app/onboarding/page.tsx
@@ -1,28 +1,37 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import { Header, Content, Typography, Button } from '@social/ui-shared';
 
 export default function Index() {
   return (
-    <>
-      <Content.Main>
-        <Header.Root>
-          <Header.Logo />
-          <Header.Title title={'Onboarding'} />
-        </Header.Root>
-        <Content.Grid>
-          <Typography.Display>You are the algorithm</Typography.Display>
-          <Typography.PageTitle className="text-opacity-50 mt-4 sm:mt-0">
-            Your keys, your content, your rules. Social content, re-imagined.
-          </Typography.PageTitle>
+    <Content.Main background="bg-black" className="pb-0">
+      <Header.Root>
+        <Header.Logo />
+        <Header.Title title={'Onboarding'} />
+      </Header.Root>
+      <Content.Grid>
+        <Typography.Display>You are the algorithm</Typography.Display>
+        <Typography.PageTitle className="text-opacity-50 mt-4 sm:mt-0">
+          Your keys, your content, your rules. Social content, re-imagined.
+        </Typography.PageTitle>
+        <div className="relative">
           <Link href="/onboarding/step1">
-            <Button.Large className="sm:w-80 w-full mt-12 relative z-10">
+            <Button.Large className="sm:w-80 w-full mt-12 relative z-20">
               Get Started
             </Button.Large>
           </Link>
-        </Content.Grid>
-        {/* <div className="absolute top-0 w-full h-full bg-[url('/images/background-image.png')] bg-no-repeat bg-bottom bg-contain" />
-        <div className="absolute top-0 w-full h-full bg-[url('/images/explosion.png')] bg-no-repeat" /> */}
-      </Content.Main>
-    </>
+          <Image
+            src="/images/explosion.png"
+            alt="explosion"
+            width={768}
+            height={768}
+            className="absolute mt-10 sm:mt-0 xl:right-96 xl:top-14 z-10"
+          />
+        </div>
+      </Content.Grid>
+      <div className="relative h-screen overflow-hidden">
+        <div className="absolute inset-0 bg-cover bg-center bg-[url('/images/background-image.png')]" />
+      </div>
+    </Content.Main>
   );
 }
