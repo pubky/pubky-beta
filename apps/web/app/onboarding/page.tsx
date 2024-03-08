44e42fc218c72c91542a86c9b84eef112686a956
Miguel Medeiros <miguel@miguelmedeiros.com.br>
2024-03-08 12:03:35 -0300

    fix: removed unnecessary wrapper

--- apps/web/app/onboarding/page.tsx
+++ apps/web/app/onboarding/page.tsx
@@ -1,39 +1,37 @@
 import Link from 'next/link';
-import { Header, Content, Typography, Button } from '@social/ui-shared';
 import Image from 'next/image';
+import { Header, Content, Typography, Button } from '@social/ui-shared';
 
 export default function Index() {
   return (
-    <>
-      <Content.Main background="bg-black" className="pb-0">
-        <Header.Root>
-          <Header.Logo />
-          <Header.Title title={'Onboarding'} />
-        </Header.Root>
-        <Content.Grid>
-          <Typography.Display>You are the algorithm</Typography.Display>
-          <Typography.PageTitle className="text-opacity-50 mt-4 sm:mt-0">
-            Your keys, your content, your rules. Social content, re-imagined.
-          </Typography.PageTitle>
-          <div className="relative">
-            <Link href="/onboarding/step1">
-              <Button.Large className="sm:w-80 w-full mt-12 relative z-20">
-                Get Started
-              </Button.Large>
-            </Link>
-            <Image
-              src="/images/explosion.png"
-              alt="explosion"
-              width={768}
-              height={768}
-              className="absolute mt-10 sm:mt-0 xl:right-96 xl:top-14 z-10"
-            />
-          </div>
-        </Content.Grid>
-        <div className="relative h-screen overflow-hidden">
-          <div className="absolute inset-0 bg-cover bg-center bg-[url('/images/background-image.png')]" />
+    <Content.Main background="bg-black" className="pb-0">
+      <Header.Root>
+        <Header.Logo />
+        <Header.Title title={'Onboarding'} />
+      </Header.Root>
+      <Content.Grid>
+        <Typography.Display>You are the algorithm</Typography.Display>
+        <Typography.PageTitle className="text-opacity-50 mt-4 sm:mt-0">
+          Your keys, your content, your rules. Social content, re-imagined.
+        </Typography.PageTitle>
+        <div className="relative">
+          <Link href="/onboarding/step1">
+            <Button.Large className="sm:w-80 w-full mt-12 relative z-20">
+              Get Started
+            </Button.Large>
+          </Link>
+          <Image
+            src="/images/explosion.png"
+            alt="explosion"
+            width={768}
+            height={768}
+            className="absolute mt-10 sm:mt-0 xl:right-96 xl:top-14 z-10"
+          />
         </div>
-      </Content.Main>
-    </>
+      </Content.Grid>
+      <div className="relative h-screen overflow-hidden">
+        <div className="absolute inset-0 bg-cover bg-center bg-[url('/images/background-image.png')]" />
+      </div>
+    </Content.Main>
   );
 }
