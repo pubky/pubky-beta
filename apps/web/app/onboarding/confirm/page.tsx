d48f27b3d6421317dc2f1c95b1cad4ebde6e52ef
Miguel Medeiros <miguel@miguelmedeiros.com.br>
2024-03-12 13:49:24 -0300

    fix: typo

--- apps/web/app/onboarding/confirm/page.tsx
+++ apps/web/app/onboarding/confirm/page.tsx
@@ -21,7 +21,7 @@ export default function Index() {
       </div>
       <div className="flex-col justify-center items-center flex">
         <Link href="/home" className="w-full sm:w-80">
-          <Button.Large>Start exploring</Button.Large>
+          <Button.Large>Start Exploring</Button.Large>
         </Link>
       </div>
     </Onboarding.Layout>
