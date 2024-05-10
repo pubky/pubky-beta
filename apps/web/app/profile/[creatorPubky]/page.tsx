0cc2d443d8f7d08a29567d4771831a1884f1fcc6
Miguel Medeiros <miguel@miguelmedeiros.com.br>
2024-05-10 15:39:57 -0300

    fix: minify name profile

--- apps/web/app/profile/[creatorPubky]/page.tsx
+++ apps/web/app/profile/[creatorPubky]/page.tsx
@@ -8,6 +8,7 @@ import { CreatePost, Header, Post, PostsLayout } from '../../components';
 import { useClientContext } from '../../../contexts/client';
 import { IPost, INewPost } from '../../../types';
 import { minifyPubky } from '../../../libs/pubkyHelper';
+import { minifyText } from '../../../libs/textHelper';
 
 export default function Index({
   params,
@@ -102,7 +103,7 @@ export default function Index({
       <div>
         <Content.Grid className="flex flex-col text-center lg:flex-row items-center sm:justify-between relative z-10">
           <ProfileCommon.Handle
-            username={name}
+            username={minifyText(name, 17)}
             className="order-2 lg:order-1"
             pubkey={minifyPubky(handler)}
           />
