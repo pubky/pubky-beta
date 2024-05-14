3147b6fd3c544573b943f5e9afec768f0eefb230
Flavio <moceri.flavio@gmail.com>
2024-05-14 20:17:33 +0200

    feat: add `tooltipProfile` to post (#207)
    * feat: add tooltipProfile to post
    
    * fix: add cursor default
    
    * fix: small css changes and change delay
    
    ---------
    
    Co-authored-by: Miguel Medeiros <miguel@miguelmedeiros.com.br>

---
+++ libs/ui-shared/src/lib/Tooltip/Tooltip.Main.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { twMerge } from 'tailwind-merge';
+
+interface MainProps extends React.HTMLAttributes<HTMLDivElement> {
+  children: React.ReactNode;
+}
+
+export const Main = ({ children, ...rest }: MainProps) => {
+  const baseCSS =
+    'w-full absolute bottom-full left-1/2 transform -translate-x-1/2 translate-y-[10px] bg-gradient-to-b from-[#07040a] to-[#1b1820] text-white px-8 py-6 rounded-[20px] shadow-md z-50';
+  return (
+    <div {...rest} className={twMerge(baseCSS, rest.className)}>
+      {children}
+    </div>
+  );
+};
--- libs/ui-shared/src/lib/Tooltip/index.tsx
+++ libs/ui-shared/src/lib/Tooltip/index.tsx
@@ -1,5 +1,9 @@
+import { Main } from './Tooltip.Main';
 import { Small } from './Tooltip.Small';
+import { Root } from './Tooltip.Root';
 
 export const Tooltip = {
+  Main,
   Small,
+  Root,
 };
