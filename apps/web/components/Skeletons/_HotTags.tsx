77b3b0fe09821bc84a1e820ef2467b6361c04fa5
flaviomoceri <moceri.flavio@gmail.com>
2024-05-15 13:35:21 +0200

    fix: fix skeleton.hotTags

--- apps/web/components/Skeletons/_HotTags.tsx
+++ apps/web/components/Skeletons/_HotTags.tsx
@@ -2,79 +2,18 @@ export default function HotTags() {
   return (
     <div className="inline-flex flex-col gap-4">
       <div className="flex gap-3">
-        <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-5"></div>
+        <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-16"></div>
         <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-24"></div>
         <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-16"></div>
-      </div>
-      <div className="flex gap-3">
-        <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-5"></div>
-        <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-36"></div>
         <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-16"></div>
+        <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-24"></div>
       </div>
       <div className="flex gap-3">
-        <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-5"></div>
+        <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-16"></div>
         <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-24"></div>
         <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-16"></div>
-      </div>
-      <div className="flex gap-3">
-        <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-5"></div>
-        <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-32"></div>
         <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-16"></div>
-      </div>
-      <div className="flex gap-3">
-        <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-5"></div>
-        <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-36"></div>
-        <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-16"></div>
-      </div>
-      <div className="flex gap-3">
-        <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-5"></div>
         <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-24"></div>
-        <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-16"></div>
-      </div>
-      <div className="flex gap-3">
-        <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-5"></div>
-        <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-36"></div>
-        <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-16"></div>
-      </div>
-      <div className="flex gap-3">
-        <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-5"></div>
-        <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-36"></div>
-        <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-16"></div>
-      </div>
-      <div className="flex gap-3">
-        <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-5"></div>
-        <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-24"></div>
-        <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-16"></div>
-      </div>
-      <div className="flex gap-3">
-        <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-5"></div>
-        <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-24"></div>
-        <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-16"></div>
-      </div>
-      <div className="flex gap-3">
-        <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-5"></div>
-        <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-24"></div>
-        <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-16"></div>
-      </div>
-      <div className="flex gap-3">
-        <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-5"></div>
-        <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-36"></div>
-        <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-16"></div>
-      </div>
-      <div className="flex gap-3">
-        <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-5"></div>
-        <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-24"></div>
-        <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-16"></div>
-      </div>
-      <div className="flex gap-3">
-        <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-5"></div>
-        <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-32"></div>
-        <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-16"></div>
-      </div>
-      <div className="flex gap-3">
-        <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-5"></div>
-        <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-36"></div>
-        <div className="h-3.5 bg-gray-300 dark:bg-gray-700 bg-opacity-50 dark:bg-opacity-50 rounded-full w-16"></div>
       </div>
     </div>
   );
