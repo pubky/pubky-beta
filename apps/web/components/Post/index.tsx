b7e4f99ae1d78d5b9f1735bac6c62144e9e2e46e
flaviomoceri <moceri.flavio@gmail.com>
2024-05-29 15:27:50 +0200

    fix: click on post

--- apps/web/components/Post/index.tsx
+++ apps/web/components/Post/index.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
+import { Utils } from '../../utils';
 import { Post as PostUI } from '@social/ui-shared';
 
 import { IPost, TLayouts, TSize } from '../../types';
@@ -25,10 +27,14 @@ export default function Post({
   fullContent = false,
   ...rest
 }: PostProps) {
+  const router = useRouter();
   return (
     <div>
       <div className="flex flex-col">
-        <PostUI.Root>
+        <PostUI.Root
+          className="cursor-pointer"
+          onClick={() => router.push(Utils.encodePostUri(post?.uri))}
+        >
           <div>
             {/* {repost && (
               <PostUI.RepostCard>
