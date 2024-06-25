d9d3a47c1157467e0254205c8cedb785990f42e5
flaviomoceri <moceri.flavio@gmail.com>
2024-06-25 18:03:11 +0200

    fix(login): manage userNotFound in the db

--- apps/web/app/sign-in/page.tsx
+++ apps/web/app/sign-in/page.tsx
@@ -40,6 +40,7 @@ export default function Index() {
     recoveryFile: '',
   });
   const [loginError, setLoginError] = useState(false);
+  const [userNotFound, setUserNotFound] = useState(false);
   const [loading, setLoading] = useState(false);
 
   const UploadRecoveryFile = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -96,8 +97,13 @@ export default function Index() {
 
         if (loggedIn) {
           router.push('/home');
+        } else if (loggedIn === undefined) {
+          setUserNotFound(true);
+          setLoginError(false);
+          setLoading(false);
         } else {
           setLoginError(true);
+          setUserNotFound(false);
           setLoading(false);
         }
       } catch (error) {
@@ -191,6 +197,23 @@ export default function Index() {
                   Recovery password or recovery file incorrect
                 </Typography.Body>
                 <Icon.Warning color="#e95164" />
+              </div>
+            )}
+            {userNotFound && (
+              <div className="flex justify-center items-center px-4 py-2 mt-6 mb-4 rounded-lg border-2 border-fuchsia-500 bg-fuchsia-500 bg-opacity-10">
+                <Typography.Body
+                  className="text-fuchsia-500"
+                  variant="small-bold"
+                >
+                  Your profile was not found, please{' '}
+                  <span
+                    onClick={() => router.push('/onboarding/sign-in')}
+                    className="text-white text-opacity-90 hover:text-opacity-100 cursor-pointer"
+                  >
+                    create a new one
+                  </span>
+                </Typography.Body>
+                <Icon.Warning color="#d946ef" />
               </div>
             )}
             <Button.Large
