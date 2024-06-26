b4a97b0f3f165fbd60e6feb92ba9902eed87cc51
flaviomoceri <moceri.flavio@gmail.com>
2024-06-26 20:18:16 +0200

    fix: inputTextArea Bio

--- apps/web/app/settings/page.tsx
+++ apps/web/app/settings/page.tsx
@@ -350,6 +350,9 @@ export default function Index() {
                   defaultValue={bio}
                   error={errors.bio}
                   onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => {
+                    if (e.target.value === '') {
+                      setBio('');
+                    }
                     if (Utils.isValidContent(e.target.value)) {
                       const cleanedBio = Utils.cleanText(e.target.value);
                       setBio(cleanedBio);
