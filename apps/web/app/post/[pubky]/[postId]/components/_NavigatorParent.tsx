06f218b6f68f1d7289fc311957ac9ef8641fff42
Flavio <moceri.flavio@gmail.com>
2024-07-29 14:37:40 +0200

    feat: replies-navigator (#343)
    * fix: duplicate/loading/increase timestamp range
    
    * feat: replies-navigator
    
    * fix: stepperReplies
    
    * fix: manage loading stepperReplies

---
+++ apps/web/app/post/[pubky]/[postId]/components/_NavigatorParent.tsx
@@ -0,0 +1,108 @@
+import { useEffect, useState } from 'react';
+import { useClientContext } from '@/contexts';
+import { Skeleton } from '@/components';
+import { IPost, IReply } from '@/types';
+import { Content } from '@social/ui-shared';
+import Skeletons from '@/components/Skeletons';
+
+interface NavigatorParentProps {
+  [uri: string]: {
+    post: IPost | null;
+    loading: boolean;
+  };
+}
+
+export default function NavigatorParent({ replies }: { replies: IReply }) {
+  const { getPost } = useClientContext();
+  const [parentURIs, setParentURIs] = useState<string[]>([]);
+  const [parentPosts, setParentPosts] = useState<NavigatorParentProps>({});
+
+  useEffect(() => {
+    const fetchParentURIs = async (
+      parentURI: string,
+      collectedURIs: string[]
+    ): Promise<string[]> => {
+      if (!parentURI) return collectedURIs;
+      collectedURIs.push(parentURI);
+      try {
+        const parentPost = await getPost(parentURI);
+        if (parentPost && parentPost.post && parentPost.post.parent) {
+          return await fetchParentURIs(parentPost.post.parent, collectedURIs);
+        }
+      } catch (error) {
+        console.error('Error fetching parent post:', error);
+      }
+      return collectedURIs;
+    };
+
+    const fetchParentPosts = async () => {
+      try {
+        if (replies?.post?.post?.parent) {
+          const parentURIList = await fetchParentURIs(
+            replies.post.post.parent,
+            []
+          );
+          setParentURIs(parentURIList);
+        }
+      } catch (error) {
+        console.error('Error fetching parent URIs:', error);
+      }
+    };
+
+    if (replies) {
+      fetchParentPosts();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [replies]);
+
+  useEffect(() => {
+    const fetchPost = async (parentURI: string) => {
+      try {
+        setParentPosts((prevState) => ({
+          ...prevState,
+          [parentURI]: { post: null, loading: true },
+        }));
+        const post = await getPost(parentURI);
+        setParentPosts((prevState) => ({
+          ...prevState,
+          [parentURI]: { post: post || null, loading: false },
+        }));
+      } catch (error) {
+        console.error('Error fetching parent post:', error);
+        setParentPosts((prevState) => ({
+          ...prevState,
+          [parentURI]: { post: null, loading: false },
+        }));
+      }
+    };
+
+    parentURIs.forEach((parentURI) => {
+      if (!parentPosts[parentURI]) {
+        fetchPost(parentURI);
+      }
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [parentURIs, parentPosts]);
+
+  const allParentPostsLoaded = parentURIs.every(
+    (uri) => parentPosts[uri]?.loading === false
+  );
+
+  if (!allParentPostsLoaded) {
+    return <Skeleton.Simple />;
+  }
+
+  return (
+    <>
+      {parentURIs && parentURIs.length > 0 ? (
+        <Content.StepperReplies
+          className="mb-4"
+          postUri={replies.post.uri}
+          urls={parentURIs.slice().reverse()}
+        />
+      ) : (
+        <Skeletons.Simple />
+      )}
+    </>
+  );
+}
--- apps/web/app/post/[pubky]/[postId]/components/index.tsx
+++ apps/web/app/post/[pubky]/[postId]/components/index.tsx
@@ -1,9 +1,11 @@
+import NavigatorParent from './_NavigatorParent';
 import Partecipants from './_Partecipants';
 import ReplyForm from './_ReplyForm';
 import Replies from './_Replies';
 import RootParent from './_RootParent';
 
 export const Post = {
+  NavigatorParent,
   Partecipants,
   ReplyForm,
   Replies,
