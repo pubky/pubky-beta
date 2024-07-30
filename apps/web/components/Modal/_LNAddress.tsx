20065c1925a3a62028b2a3d61b73c9e7359d6b33
flaviomoceri <moceri.flavio@gmail.com>
2024-07-30 16:10:38 +0200

    feat: add qrCode

--- apps/web/components/Modal/_LNAddress.tsx
+++ apps/web/components/Modal/_LNAddress.tsx
@@ -1,7 +1,8 @@
 'use client';
 
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Button, Icon, Modal, Typography } from '@social/ui-shared';
+import QRCode from 'qrcode.react';
 
 interface LNAddressProps {
   showModal: boolean;
@@ -15,6 +16,19 @@ export default function LNAddress({
   lnAddress,
 }: LNAddressProps) {
   const modalRef = useRef<HTMLDivElement>(null);
+  const [copiedLNAddress, setCopiedLNAddress] = useState(false);
+
+  const handleCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(lnAddress || '');
+      setCopiedLNAddress(true);
+      setTimeout(() => {
+        setCopiedLNAddress(false);
+      }, 1000);
+    } catch (error) {
+      console.log('Failed to copy: ', error);
+    }
+  };
 
   useEffect(() => {
     const handleClickOutsideModal = (event: MouseEvent) => {
@@ -37,19 +51,43 @@ export default function LNAddress({
       show={showModal}
       closeModal={() => setShowModal(false)}
       modalRef={modalRef}
-      className="w-[588px]"
+      className="w-[450px]"
     >
       <Modal.CloseAction onClick={() => setShowModal(false)} />
-      <Typography.Body variant="medium" className="text-opacity-80">
-        {lnAddress}
-      </Typography.Body>
-      <Button.Medium
-        variant="line"
-        className="mt-4"
-        icon={<Icon.File size="16" />}
-      >
-        Copy lightning address
-      </Button.Medium>
+      <div className="flex flex-col items-center">
+        {lnAddress && (
+          <div onClick={handleCopy} className="relative cursor-pointer">
+            <QRCode
+              value={lnAddress}
+              size={256}
+              level="H"
+              className="rounded-lg"
+              includeMargin={true}
+              renderAs="svg"
+            />
+            <div
+              className="absolute inset-0 flex items-center justify-center"
+            >
+              <div className="p-2 bg-black rounded-full">
+                <Icon.Lightning size="24" color="#D946EF" />
+              </div>
+            </div>
+          </div>
+        )}
+        <Typography.Body variant="medium" className="text-opacity-80 mt-4">
+          {lnAddress}
+        </Typography.Body>
+        <Button.Medium
+          variant="line"
+          className="mt-4"
+          icon={
+            copiedLNAddress ? <Icon.Check size="16" /> : <Icon.File size="16" />
+          }
+          onClick={handleCopy}
+        >
+          {copiedLNAddress ? 'Copied' : 'Copy lightning address'}
+        </Button.Medium>
+      </div>
     </Modal.Root>
   );
 }
