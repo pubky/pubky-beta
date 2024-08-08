f4f0328a537d2535ddf652b905a8d76fdf58fca7
flaviomoceri <moceri.flavio@gmail.com>
2024-08-08 11:30:37 +0200

    refactor: post page

--- apps/web/app/post/[pubky]/[postId]/page.tsx
+++ apps/web/app/post/[pubky]/[postId]/page.tsx
@@ -49,60 +49,67 @@ export default function Index({
       setContent('Reply created!');
       setShow(true);
     } else {
-      setContent('Something wrong. Try again', 'warning');
+      setContent('Something went wrong. Try again', 'warning');
       setShow(true);
     }
   };
+
+  let content;
+
+  if (!showPost) {
+    content = (
+      <div className="ml-4 px-6 py-2 bg-white bg-opacity-10 rounded-2xl">
+        <Typography.Body
+          variant="small"
+          className="text-opacity-50 text-center"
+        >
+          This post was not found or has been deleted by its author.
+          <Link
+            href="/home"
+            className="ml-2 text-fuchsia-500 text-opacity-80 hover:text-opacity-100 cursor-pointer"
+          >
+            Go home
+          </Link>
+        </Typography.Body>
+      </div>
+    );
+  } else {
+    content = (
+      <>
+        {replies?.post?.post?.parent && (
+          <Post.NavigatorParent replies={replies} />
+        )}
+
+        {loading ? (
+          <Skeletons.Simple />
+        ) : (
+          <>
+            <PostComponent
+              key={uri}
+              post={post}
+              size="full"
+              largeView={true}
+              fullContent
+            />
+            <div className="mt-3">
+              <Post.ReplyForm
+                uri={uri}
+                post={post}
+                updatePost={handleUpdatePost}
+                replies={replies}
+              />
+            </div>
+          </>
+        )}
+      </>
+    );
+  }
 
   return (
     <Content.Main>
       <Header className="hidden md:block" title="Post" />
       <Content.Grid className="flex justify-between flex-col gap-3">
-        {showPost ? (
-          <>
-            {replies?.post?.post?.parent && (
-              <Post.NavigatorParent replies={replies} />
-            )}
-
-            {loading && <Skeletons.Simple />}
-
-            {!loading && (
-              <>
-                <PostComponent
-                  key={uri}
-                  post={post}
-                  size="full"
-                  largeView={true}
-                  fullContent
-                  //line={replies?.post?.post?.parent ? true : false}
-                />
-                <div className="mt-3">
-                  <Post.ReplyForm
-                    uri={uri}
-                    post={post}
-                    updatePost={handleUpdatePost}
-                    replies={replies}
-                  />
-                </div>
-              </>
-            )}
-          </>
-        ) : (
-          <div className="ml-4 px-6 py-2 bg-white bg-opacity-10 rounded-2xl">
-            <Typography.Body
-              variant="small"
-              className="text-opacity-50 text-center"
-            >
-              This post was not found or has been deleted by its author.
-              <Link
-                href="/home"
-                className="ml-2 text-fuchsia-500 text-opacity-80 hover:text-opacity-100 cursor-pointer"
-              >
-                Go home
-              </Link>
-            </Typography.Body>
-          </div>
-        )}
+        {content}
       </Content.Grid>
       <CreatePost />
     </Content.Main>
