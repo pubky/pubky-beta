739b69ea1cec4f9d6a4e8a3d2a345f711281b0a9
flaviomoceri <moceri.flavio@gmail.com>
2024-09-02 16:44:37 +0200

    feat (nexus): add version info to settings pase

--- apps/web/app/settings/_version.tsx
+++ apps/web/app/settings/_version.tsx
@@ -1,12 +1,20 @@
 import { SideCard, Typography } from '@social/ui-shared';
+import { useServerInfo } from '../../hooks/useServerInfo';
+import { Skeleton } from '@/components';
 
 export default function Version() {
+  const { data, isLoading } = useServerInfo();
+
   return (
     <div className="w-full flex-col justify-start items-start gap-2 inline-flex mt-6">
       <SideCard.Header title="Version" />
-      <Typography.Body variant="medium" className="text-opacity-80">
-        Pubky version v0.12 © Synonym Software Ltd
-      </Typography.Body>
+      {isLoading ? (
+        <Skeleton.Simple />
+      ) : (
+        <Typography.Body variant="medium" className="text-opacity-80">
+          Pubky version v{data?.version} © Synonym Software Ltd
+        </Typography.Body>
+      )}
     </div>
   );
 }
