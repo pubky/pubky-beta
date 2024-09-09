9585cd1f9dcc22f6d99bb6ce43dfb4643edade6f
James Browning <jamesbrowning91@gmail.com>
2024-09-09 16:48:59 +0100

    tests: can post with image upload (skipped for now due to image src issue)

--- apps/web-e2e/src/e2e/posts.cy.ts
+++ apps/web-e2e/src/e2e/posts.cy.ts
@@ -1,5 +1,6 @@
 import { backupDownloadFilePath } from '../support/auth';
 import { slowCypressDown } from 'cypress-slow-down';
+import { selectEmoji, latestPostInFeedContentEq } from '../support/posts';
 
 describe('posts', () => {
   before(() => {
@@ -29,10 +30,7 @@ describe('posts', () => {
     });
 
     // verify the post is displayed correctly in feed
-    // second child is the latest post
-    cy.get('#posts-feed').children().eq(1).within(() => {
-      cy.innerTextShouldEq('#post-content-text', postContent);
-    });
+    latestPostInFeedContentEq(postContent);
   });
 
   it('can post from new post', () => {
@@ -50,9 +48,7 @@ describe('posts', () => {
     cy.get('#modal-root').should('not.exist');
 
     // verify the post is displayed correctly in feed
-    cy.get('#posts-feed').children().eq(1).within(() => {
-      cy.innerTextShouldEq('#post-content-text', postContent);
-    });
+    latestPostInFeedContentEq(postContent);
   });
 
   it('can post with maximum character limit (300)', () => {
@@ -71,10 +67,9 @@ describe('posts', () => {
     });
 
     // verify the post is displayed correctly in feed
-    cy.get('#posts-feed').children().eq(1).within(() => {
-      cy.innerTextShouldEq('#post-content-text', postContent);
-    });
+    latestPostInFeedContentEq(postContent);
   });
+
   it('can post with emojis', () => {
     const postContent = `🇦🇺😎🦎 I can post with emojis! ${Date.now()}`;
     const postContentWithoutEmoji = ` I can post with emojis! ${Date.now()}`;
@@ -84,21 +79,9 @@ describe('posts', () => {
       cy.get('textarea').click();
 
       // add emojis using emoji picker
-      cy.get('#emoji-btn').click();
-      cy.get('#emoji-picker').should('be.visible');
-      cy.get('#emoji-picker').within(() => {
-        cy.get('button[data-full-name*="australia flag"]').click();
-      })
-      cy.get('#emoji-btn').click();
-      cy.get('#emoji-picker').should('be.visible');
-      cy.get('#emoji-picker').within(() => {
-        cy.get('button[data-full-name*="smiling face with sunglasses"]').click();
-      })
-      cy.get('#emoji-btn').click();
-      cy.get('#emoji-picker').should('be.visible');
-      cy.get('#emoji-picker').within(() => {
-        cy.get('button[data-full-name*="lizard"]').click();
-      })
+      selectEmoji('australia flag');
+      selectEmoji('smiling face with sunglasses');
+      selectEmoji('lizard');
 
       // type the rest of the post and submit
       cy.get('textarea').type(postContentWithoutEmoji);
@@ -106,13 +89,36 @@ describe('posts', () => {
     });
 
     // verify the post is displayed correctly in feed
-    cy.get('#posts-feed').children().eq(1).within(() => {
-      cy.innerTextShouldEq('#post-content-text', postContent);
-    });
+    latestPostInFeedContentEq(postContent);
   });
 
+  // test skipped because viewing uploaded image in post doesn't work for this local deployment
+  it.skip('can post with image upload', () => {
+    const postContent = `I can post with an image! ${Date.now()}`;
+    cy.get('#quick-post-create-content').within(() => {
+      cy.get('textarea').should('have.value', '');
+      // click on textarea to expand to view buttons
+      cy.get('textarea').click();
 
-  it.skip('can post with image upload', () => { });
+      // upload image
+      cy.get('#media-upload-btn').within(() => {
+        const imagePath = Cypress.config('fixturesFolder') + '/mustache-you.png';
+        cy.get('#fileInput').selectFile(
+          imagePath,
+          { force: true } // force to bypass visibility check of hidden input field
+        );
+      });
+
+      // type the rest of the post and submit
+      cy.get('textarea').type(postContent);
+      cy.get('#post-button').click();
+    });
+
+    // todo: verify the image is displayed in the post (fails to display uploaded image here)
+
+    // verify the post is displayed correctly in feed
+    latestPostInFeedContentEq(postContent);
+  });
   it.skip('can post with embedded link', () => { });
   it.skip('can post with profile reference', () => { });
 
--- apps/web/components/CreateContent/Section/_FooterArea.tsx
+++ apps/web/components/CreateContent/Section/_FooterArea.tsx
@@ -162,6 +162,7 @@ export default function FooterArea({
             }}
           />
           <Button.Action
+            id='media-upload-btn'
             variant="custom"
             icon={<Icon.ImageSquare size="32" />}
             onClick={() => document.getElementById('fileInput')?.click()}
