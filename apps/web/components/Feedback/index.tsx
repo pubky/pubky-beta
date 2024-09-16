e4a16c085ed94820f9dfd037b5277aaa3d1aa11c
Miguel Medeiros <miguel@miguelmedeiros.com.br>
2024-09-16 20:03:34 -0300

    fix: compiling issues

--- apps/web/components/Feedback/index.tsx
+++ apps/web/components/Feedback/index.tsx
@@ -1,8 +1,6 @@
 'use client';
 
-import { useClientContext } from '@/contexts';
-import { IProfile } from '@/types';
-import { Input, SideCard, Typography } from '@social/ui-shared';
+import { SideCard, Typography } from '@social/ui-shared';
 import { Utils } from '@social/utils-shared';
 import { useState } from 'react';
 import { ImageByUri } from '../ImageByUri';
@@ -42,33 +40,26 @@ export default function Feedback() {
   };
 
   return (
-    <div className="self-start sticky top-[120px] col-span-1">
-      <SideCard.Header title="Feedback" />
-      <SideCard.Content>
-        <div className="p-6 w-full rounded-lg border-dashed border border-white border-opacity-30 flex-col justify-start items-start inline-flex">
-          <div className="flex flex-col gap-3">
-            <div className="flex gap-2 items-center">
-              <ImageByUri
-                alt="user"
-                uri={profile?.details?.image ?? '/images/Userpic.png'}
-                width={32}
-                height={32}
-                className="rounded-full w-8 h-8"
-              />
-              <Typography.Body variant="medium-bold">
-                {Utils.minifyText(profile?.details?.name ?? 'Loading...', 10)}
-              </Typography.Body>
+    <>
+      <div className="self-start sticky top-[120px] col-span-1">
+        <SideCard.Header title="Feedback" />
+        <SideCard.Content>
+          <div className="p-6 w-full rounded-lg border-dashed border border-white border-opacity-30 flex-col justify-start items-start inline-flex">
+            <div className="flex flex-col gap-3">
+              <div className="flex gap-2 items-center">
+                <ImageByUri
+                  alt="user"
+                  uri={profile?.details?.image ?? '/images/Userpic.png'}
+                  width={32}
+                  height={32}
+                  className="rounded-full w-8 h-8"
+                />
+                <Typography.Body variant="medium-bold">
+                  {Utils.minifyText(profile?.details?.name ?? 'Loading...', 10)}
+                </Typography.Body>
+              </div>
             </div>
           </div>
-          {/**
-        <SideCard.Action
-          icon={<Icon.Envelope size="16" color="gray" />}
-          disabled={!message}
-          onClick={() => (message ? handleSubmit() : undefined)}
-          className="mt-4"
-          text="Submit Feedback"
-        />
-        */}
         </SideCard.Content>
       </div>
       <Modal.Feedback
