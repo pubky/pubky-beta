272e9500ba2328eb692ce184459f2476f26f3e03
James Browning <jamesbrowning91@gmail.com>
2024-11-07 12:07:41 +0000

    use prod build of pubky-app

--- apps/web-e2e/src/e2e/settings.cy.ts
+++ apps/web-e2e/src/e2e/settings.cy.ts
@@ -1,4 +1,3 @@
-import { editProfileAndVerify, addLinks } from '../support/profile';
 import { slowCypressDown } from 'cypress-slow-down';
 
 describe('settings', () => {
@@ -10,55 +9,55 @@ describe('settings', () => {
     cy.onboardAsNewUser('Mr Settings', 'I like to change settings');
   });
 
-  it('Account settings is displayed correctly', () => {
+  it.skip('Account settings is displayed correctly', () => {
 
   });
 
-  it('Account settings: can back up account', () => {
+  it.skip('Account settings: can back up account', () => {
 
   });
 
-  it('Account settings: can delete account', () => {
+  it.skip('Account settings: can delete account', () => {
 
   });
 
-  it('Account settings: can edit profile', () => {
+  it.skip('Account settings: can edit profile', () => {
 
   });
 
-  it('Account settings: can download your data', () => {
+  it.skip('Account settings: can download your data', () => {
 
   });
 
-  it('Notifications settings is displayed correctly', () => {
+  it.skip('Notifications settings is displayed correctly', () => {
 
   });
 
   // TODO: add Notifications tests in a separate file
 
-  it('Privacy and Safety settings is displayed correctly', () => {
+  it.skip('Privacy and Safety settings is displayed correctly', () => {
 
   });
 
   // TODO: add Privacy and Safety tests in a separate file
 
-  it('Muted users settings displays muted users', () => {
+  it.skip('Muted users settings displays muted users', () => {
 
   });
 
-  it('Language settings:new language can be selected', () => {
+  it.skip('Language settings:new language can be selected', () => {
 
   });
 
-  it('Help: FAQ is displayed correctly', () => {
+  it.skip('Help: FAQ is displayed correctly', () => {
 
   });
 
-  it('Help: User Guide can be navigated to', () => {
+  it.skip('Help: User Guide can be navigated to', () => {
 
   });
 
-  it('Help: Support link can be used', () => {
+  it.skip('Help: Support link can be used', () => {
 
   });
 
