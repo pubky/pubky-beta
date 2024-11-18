2a21eb8a5df837b68caa5cadfb83f0bc2581c2d5
Miguel Medeiros <miguel@miguelmedeiros.com.br>
2024-11-18 14:06:39 -0300

    fix: logout errors

--- apps/web/app/logout/components/_Content.tsx
+++ apps/web/app/logout/components/_Content.tsx
@@ -4,17 +4,11 @@ import { Button, Content, Header, Icon, Typography } from '@social/ui-shared';
 import { usePubkyClientContext } from '@/contexts';
 import Link from 'next/link';
 import Image from 'next/image';
-import { useEffect } from 'react';
 
 export default function Index() {
   const { logout } = usePubkyClientContext();
 
-  useEffect(() => {
-    async function funcLogout() {
-      await logout();
-    }
-    funcLogout();
-  }, [logout]);
+  logout();
 
   return (
     <Content.Main>
