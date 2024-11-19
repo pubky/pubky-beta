5fc892a3de718fbe88bc374e67dfba015c1ee996
Miguel Medeiros <miguel@miguelmedeiros.com.br>
2024-11-19 09:18:49 -0300

    fix: remove unnecessary background opacity in Counter component

--- libs/ui-shared/src/lib/PostUtil/_Counter.tsx
+++ libs/ui-shared/src/lib/PostUtil/_Counter.tsx
@@ -8,7 +8,7 @@ interface CounterProps extends React.HTMLAttributes<HTMLDivElement> {
 }
 
 export const Counter = ({ children, textCSS, ...rest }: CounterProps) => {
-  const baseCSS = `leading-none w-8 h-8 px-3 py-1.5 bg-black bg-opacity-90 rounded-[32px] border border-white border-opacity-30 flex-col justify-center items-center gap-2 inline-flex`;
+  const baseCSS = `leading-none w-8 h-8 px-3 py-1.5 bg-black rounded-[32px] border border-white border-opacity-30 flex-col justify-center items-center gap-2 inline-flex`;
 
   return (
     <div {...rest} className={twMerge(baseCSS, rest.className)}>
