057863532de2e10af1191da27587a6c854b5782c
Miguel Medeiros <miguel@miguelmedeiros.com.br>
2024-11-30 18:39:21 -0300

    fix: update modal layout and structure

--- libs/ui-shared/src/lib/Modal/_Root.tsx
+++ libs/ui-shared/src/lib/Modal/_Root.tsx
@@ -19,12 +19,12 @@ export const Root = ({
     <div className="flex justify-center items-center">
       {show && (
         <div className="fixed top-0 left-0 z-50 w-full h-full bg-black bg-opacity-70 flex justify-center items-center">
-          <div id='modal-root' ref={modalRef}>
+          <div id="modal-root" ref={modalRef}>
             <Card.Primary
               background="bg-[#05050A] opacity-100"
               className={twMerge(
-                `w-full h-full p-12 rounded-2xl shadow border border-white border-opacity-30 flex-col inline-flex`,
-                rest.className
+                `w-full h-full p-12 rounded-2xl shadow border border-white border-opacity-30 inline-flex flex-row`,
+                rest.className,
               )}
             >
               {children}
