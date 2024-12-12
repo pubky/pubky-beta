d0fa0ce5462a5f8a19349aa2783008778350f4f6
James Browning <jamesbrowning91@gmail.com>
2024-12-12 19:26:58 +0000

    test: add filter by friend e2e test

--- apps/web-e2e/src/e2e/feed.cy.ts
+++ apps/web-e2e/src/e2e/feed.cy.ts
@@ -83,7 +83,7 @@ describe('feed and filters', () => {
 
     cy.signOut(true);
 
-    // sign in as profile 4 and view Reach All posts, all can be seen
+    // * sign in as profile 4 and view Reach All posts, all can be seen
     cy.signIn(backupDownloadFilePath(`${profile4.username}.pkarr`));
     // Reach All is the default view so no need to click
 
@@ -96,13 +96,12 @@ describe('feed and filters', () => {
     cy.findPostInFeed(profile4.postText).should('be.visible');
   });
 
-  it.only('can filter to view only posts and reposts of following', () => {
-    // * sign in as profile 2 and view Reach Following, profile 1's posts can be seen
+  it('can filter to view only posts and reposts of following', () => {
+    // * sign in as profile 2 and view Reach Following, only profile 1's posts can be seen
     cy.signIn(backupDownloadFilePath(`${profile2.username}.pkarr`));
     // click the following button in the leftmost (first) sidebar item
     cy.get('#sidebar').first().find('#reach-following-btn').click();
 
-    // check only profile 1's posts are visible
     cy.findPostInFeed(profile1.postText1).should('be.visible');
     cy.findPostInFeed(profile1.postText2).should('be.visible');
     cannotFindPostInFeed(profile2.postText);
@@ -138,6 +137,43 @@ describe('feed and filters', () => {
     cy.get('#posts-feed').find('#timeline').should('contain.text', 'No posts yet');
   });
 
-  it.skip('can filter view only posts and reposts of friends')
+  it('can filter view only posts and reposts of friends', () => {
+    // * sign in as profile 1 and view Reach Friends, only profile 2's post can be seen
+    cy.signIn(backupDownloadFilePath(`${profile1.username}.pkarr`));
+    cy.get('#sidebar').first().find('#reach-friends-btn').click();
+
+    cy.findPostInFeed(profile2.postText).should('be.visible');
+    cy.findPostInFeed(profile2.repostText).should('be.visible');
+    countPostsInFeed(profile1.postText2, 1); // 1 occurrence due to profile 2 reposting profile 1's post
+    cannotFindPostInFeed(profile1.postText1);
+    cannotFindPostInFeed(profile3.postText);
+    cannotFindPostInFeed(profile4.postText);
+
+    cy.signOut(true);
+
+    // * sign in as profile 2 and view Reach Friends, only profile 1's posts can be seen
+    cy.signIn(backupDownloadFilePath(`${profile2.username}.pkarr`));
+    cy.get('#sidebar').first().find('#reach-friends-btn').click();
+
+    cy.findPostInFeed(profile1.postText1).should('be.visible');
+    cy.findPostInFeed(profile1.postText2).should('be.visible');
+    cannotFindPostInFeed(profile2.postText);
+    cannotFindPostInFeed(profile2.repostText);
+    cannotFindPostInFeed(profile3.postText);
+    cannotFindPostInFeed(profile4.postText);
+
+    cy.signOut(true);
+
+    // * sign in as profile 3 and view Reach Friends, no posts can be seen
+    cy.signIn(backupDownloadFilePath(`${profile3.username}.pkarr`));
+    cy.get('#sidebar').first().find('#reach-friends-btn').click();
+
+    cannotFindPostInFeed(profile1.postText1);
+    cannotFindPostInFeed(profile1.postText2);
+    cannotFindPostInFeed(profile2.postText);
+    cannotFindPostInFeed(profile2.repostText);
+    cannotFindPostInFeed(profile3.postText);
+    cannotFindPostInFeed(profile4.postText);
+  });
   it.skip('can sort by ')
 });
