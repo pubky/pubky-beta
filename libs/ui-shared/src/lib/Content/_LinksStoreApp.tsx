bd77aea936a72ae9199209057501955401f7f586
flaviomoceri <moceri.flavio@gmail.com>
2024-12-24 12:10:02 +0100

    qrCode auth with Pubky Ring

--- libs/ui-shared/src/lib/Content/_LinksStoreApp.tsx
+++ libs/ui-shared/src/lib/Content/_LinksStoreApp.tsx
@@ -7,36 +7,14 @@ interface LinksStoreAppProps extends React.HTMLAttributes<HTMLDivElement> {
 }
 
 export const LinksStoreApp = ({ ...rest }: LinksStoreAppProps) => {
-  const baseCSS = 'flex mt-4';
+  const baseCSS = '-ml-2 mt-4 self-stretch items-center inline-flex';
   return (
     <div {...rest} className={twMerge(baseCSS, rest.className)}>
       {/**<Link href="https://www.bitkit.to/" target="_blank">
-        <Image width={122} height={36} alt="bitkit" src="/images/webp/bitkit.webp" />
-      </Link>*/}
-      <Link
-        href="https://apps.apple.com/us/app/bitkit-wallet/id6502440655"
-        target="_blank"
-      >
-        <Image
-          width={160}
-          height={94}
-          alt="apple-store"
-          className="w-full h-auto"
-          src="/images/webp/apple-store.webp"
-        />
-      </Link>
-      <Link
-        href="https://play.google.com/store/apps/details?id=to.bitkit"
-        target="_blank"
-      >
-        <Image
-          width={160}
-          height={94}
-          alt="google-play"
-          className="w-full h-auto"
-          src="/images/webp/google-play.webp"
-        />
-      </Link>
+      <Image width={122} height={36} alt="bitkit" src="/images/webp/bitkit.webp" />
+    </Link>*/}
+      <img className="w-[112px] h-[45px]" src="/images/webp/apple-store.webp" />
+      <img className="w-[112px] h-[45px]" src="/images/webp/google-play.webp" />
     </div>
   );
 };
