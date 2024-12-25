f10d8321194088a635ae87c4c788f3e564b9030f
Miguel Medeiros <miguel@miguelmedeiros.com.br>
2024-12-25 12:12:21 -0300

    refactor: remove unused imports in LinksStoreApp component

--- libs/ui-shared/src/lib/Content/_LinksStoreApp.tsx
+++ libs/ui-shared/src/lib/Content/_LinksStoreApp.tsx
@@ -1,5 +1,3 @@
-import Image from 'next/image';
-import Link from 'next/link';
 import { twMerge } from 'tailwind-merge';
 
 interface LinksStoreAppProps extends React.HTMLAttributes<HTMLDivElement> {
@@ -10,9 +8,6 @@ export const LinksStoreApp = ({ ...rest }: LinksStoreAppProps) => {
   const baseCSS = '-ml-2 mt-4 self-stretch items-center inline-flex';
   return (
     <div {...rest} className={twMerge(baseCSS, rest.className)}>
-      {/**<Link href="https://www.bitkit.to/" target="_blank">
-      <Image width={122} height={36} alt="bitkit" src="/images/webp/bitkit.webp" />
-    </Link>*/}
       <img className="w-[112px] h-[45px]" src="/images/webp/apple-store.webp" />
       <img className="w-[112px] h-[45px]" src="/images/webp/google-play.webp" />
     </div>
