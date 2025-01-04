6d6f24b371722eb753e4bbbd15aa6366934f2954
flaviomoceri <moceri.flavio@gmail.com>
2025-01-04 11:23:37 +0100

    refactor editPost modal/sheet

--- apps/web/components/BottomSheet/_EditPost.tsx
+++ apps/web/components/BottomSheet/_EditPost.tsx
@@ -1,10 +1,8 @@
 'use client';
 
-import { BottomSheet, Button, Icon } from '@social/ui-shared';
-import React, { useEffect, useState } from 'react';
-import CreateContent from '../CreateContent';
-import { useAlertContext, usePubkyClientContext } from '@/contexts';
+import { BottomSheet } from '@social/ui-shared';
 import { PostView } from '@/types/Post';
+import ContentEditPost from '../Modal/_EditPost/_Content';
 
 interface EditPostProps {
   show: boolean;
@@ -21,39 +19,6 @@ export default function EditPost({
   title,
   className,
 }: EditPostProps) {
-  const { editPost } = usePubkyClientContext();
-  const { addAlert } = useAlertContext();
-  const [contentEditPost, setContentEditPost] = useState('');
-  const [sendingEditPost, setSendingEditPost] = useState(false);
-  const [isValidContent, setIsValidContent] = useState(false);
-
-  useEffect(() => {
-    setContentEditPost(post?.details?.content);
-  }, [post]);
-
-  const handleSubmit = async (content: string) => {
-    if (sendingEditPost) {
-      return;
-    }
-    try {
-      setSendingEditPost(true);
-
-      const editPostUser = await editPost(post, content);
-
-      if (editPostUser) {
-        addAlert('Post edited!');
-      } else {
-        addAlert('Something wrong. Try again', 'warning');
-      }
-      setContentEditPost('');
-      setShow(false);
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setSendingEditPost(false);
-    }
-  };
-
   return (
     <BottomSheet.Root
       show={show}
@@ -61,43 +26,7 @@ export default function EditPost({
       title={title}
       className={className}
     >
-      <div className="flex items-center relative">
-        <div className="w-full">
-          <CreateContent
-            id="new-post-create-content"
-            handleSubmit={handleSubmit}
-            content={contentEditPost}
-            placeHolder="Edit post"
-            setContent={setContentEditPost}
-            isValidContent={isValidContent}
-            setIsValidContent={setIsValidContent}
-            loading={sendingEditPost}
-            button={
-              <Button.Medium
-                id="post-btn"
-                className="w-auto"
-                variant="line"
-                icon={
-                  <Icon.PaperPlaneRight
-                    color={!isValidContent ? 'gray' : 'white'}
-                  />
-                }
-                disabled={!isValidContent}
-                loading={sendingEditPost}
-                onClick={
-                  isValidContent
-                    ? () => handleSubmit(contentEditPost)
-                    : undefined
-                }
-              >
-                Edit
-              </Button.Medium>
-            }
-            autoFocus
-            visibleTextArea
-          />
-        </div>
-      </div>
+      <ContentEditPost setShowModalEditPost={setShow} post={post} />
     </BottomSheet.Root>
   );
 }
