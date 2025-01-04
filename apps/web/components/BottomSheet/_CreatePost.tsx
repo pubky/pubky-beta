5330851dca009cce242265125fb1813cb62c360a
flaviomoceri <moceri.flavio@gmail.com>
2025-01-04 10:35:46 +0100

    refactor createPost modal/sheet

--- apps/web/components/BottomSheet/_CreatePost.tsx
+++ apps/web/components/BottomSheet/_CreatePost.tsx
@@ -1,10 +1,7 @@
 'use client';
 
-import { BottomSheet, Button, Icon } from '@social/ui-shared';
-import React, { useEffect, useState } from 'react';
-import CreateContent from '../CreateContent';
-import { useAlertContext, usePubkyClientContext } from '@/contexts';
-import { Utils } from '@social/utils-shared';
+import { BottomSheet } from '@social/ui-shared';
+import ContentCreatePost from '../Modal/_CreatePost/_Content';
 
 interface CreatePostProps {
   show: boolean;
@@ -19,58 +16,6 @@ export default function CreatePost({
   title,
   className,
 }: CreatePostProps) {
-  const { pubky, createPost, createTag } = usePubkyClientContext();
-  const { addAlert } = useAlertContext();
-  const [contentPost, setContentPost] = useState('');
-  const [sendingPost, setSendingPost] = useState(false);
-  const [arrayTags, setArrayTags] = useState<string[]>([]);
-  const [isValidContent, setIsValidContent] = useState(false);
-  const [quote, setQuote] = useState<string>();
-  const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
-  const [placeholder, setPlaceholder] = useState('');
-  const regex =
-    /pubky:\/\/([a-zA-Z0-9]+)\/pub\/pubky\.app\/posts\/([a-zA-Z0-9]+)/;
-
-  useEffect(() => {
-    setPlaceholder(Utils.promptPlaceholder('post'));
-  }, []);
-
-  const handleSubmit = async (content: string) => {
-    if (sendingPost) {
-      return;
-    }
-    try {
-      setSendingPost(true);
-
-      const hashtags = Utils.extractHashtags(content);
-      const updatedTags = [...new Set([...arrayTags, ...hashtags])];
-
-      const newPost = await createPost(content, 'short', selectedFiles, quote);
-      const match = newPost && newPost?.uri.match(regex);
-
-      if (newPost && match) {
-        const postId = match[2];
-        for (const tag of updatedTags) {
-          await createTag(pubky ?? '', postId, tag);
-        }
-
-        addAlert('Post created!');
-      } else {
-        addAlert('Something wrong. Try again', 'warning');
-      }
-      setArrayTags([]);
-      setContentPost('');
-      setShow(false);
-      setSelectedFiles([]);
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setSendingPost(false);
-    }
-  };
-
-  if (!show) return null;
-
   return (
     <BottomSheet.Root
       show={show}
@@ -78,52 +23,7 @@ export default function CreatePost({
       title={title}
       className={className}
     >
-      <div className="w-full">
-        <CreateContent
-          id="new-post-create-content"
-          handleSubmit={handleSubmit}
-          content={contentPost}
-          setContent={setContentPost}
-          placeHolder={placeholder}
-          setQuote={setQuote}
-          isValidContent={isValidContent}
-          setIsValidContent={setIsValidContent}
-          selectedFiles={selectedFiles}
-          setSelectedFiles={setSelectedFiles}
-          loading={sendingPost}
-          arrayTags={arrayTags}
-          setArrayTags={setArrayTags}
-          setShowModalPost={setShow}
-          article
-          button={
-            <Button.Medium
-              id="post-btn"
-              className="w-auto"
-              variant="line"
-              icon={
-                <Icon.PaperPlaneRight
-                  color={
-                    !isValidContent && selectedFiles.length === 0
-                      ? 'gray'
-                      : 'white'
-                  }
-                />
-              }
-              disabled={!isValidContent && selectedFiles.length === 0}
-              loading={sendingPost}
-              onClick={
-                (isValidContent || selectedFiles.length > 0) && !sendingPost
-                  ? () => handleSubmit(contentPost)
-                  : undefined
-              }
-            >
-              Post
-            </Button.Medium>
-          }
-          autoFocus
-          visibleTextArea
-        />
-      </div>
+      <ContentCreatePost setShowModalPost={setShow} />
     </BottomSheet.Root>
   );
 }
---
+++ apps/web/components/Modal/_CreatePost/index.tsx
@@ -0,0 +1,40 @@
+import { Modal } from '@social/ui-shared';
+import ContentCreatePost from './_Content';
+
+interface CreatePostProps {
+  showModalPost: boolean;
+  setShowModalPost: React.Dispatch<React.SetStateAction<boolean>>;
+  modalPostRef: React.RefObject<HTMLDivElement>;
+}
+
+export default function CreatePost({
+  showModalPost,
+  setShowModalPost,
+  modalPostRef,
+}: CreatePostProps) {
+  return (
+    <Modal.Root
+      modalRef={modalPostRef}
+      show={showModalPost}
+      closeModal={() => {
+        setShowModalPost(false);
+        //setArrayTags([]);
+      }}
+      className="md:w-[792px] max-h-[600px] overflow-y-auto max-w-[1200px]"
+    >
+      <Modal.CloseAction
+        onClick={() => {
+          setShowModalPost(false);
+          //setArrayTags([]);
+          //setContent('');
+        }}
+      />
+      <div className="flex flex-col gap-4">
+        <Modal.Header title="New Post" />
+        <div className="flex items-center relative">
+          <ContentCreatePost setShowModalPost={setShowModalPost} />
+        </div>
+      </div>
+    </Modal.Root>
+  );
+}
