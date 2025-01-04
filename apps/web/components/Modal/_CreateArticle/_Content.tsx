e5fe6c4384c46aa5328b3b3bc0d6821a2389e7e1
flaviomoceri <moceri.flavio@gmail.com>
2025-01-04 10:20:22 +0100

    refactor createarticle modal/sheet

--- apps/web/components/Modal/_CreateArticle/_Content.tsx
+++ apps/web/components/Modal/_CreateArticle/_Content.tsx
@@ -1,33 +1,27 @@
-import {
-  Button,
-  Icon,
-  Input,
-  Modal as ModalUI,
-  Typography,
-} from '@social/ui-shared';
+import { Button, Icon, Input, Typography } from '@social/ui-shared';
 import { useEffect, useRef, useState } from 'react';
 import { useAlertContext, usePubkyClientContext } from '@/contexts';
 import { Utils } from '@social/utils-shared';
-import { Section } from '../CreateContent/Section';
-import LinkPreviewer from '../LinkPreview';
+
 import { UserView } from '@/types/User';
 import Image from 'next/image';
 import { useStreamSearchUsersByUsername } from '@/hooks/useStream';
+import { Section } from '@/components/CreateContent/Section';
+import LinkPreviewer from '@/components/LinkPreview';
+import { useIsMobile } from '@/hooks/useIsMobile';
 
 interface CreateArticleProps {
-  showModalArticle: boolean;
   setShowModalArticle: React.Dispatch<React.SetStateAction<boolean>>;
   setShowModalPost?: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-export default function CreateArticle({
-  showModalArticle,
+export default function ContentCreateArticle({
   setShowModalArticle,
   setShowModalPost,
 }: CreateArticleProps) {
   const { pubky, createArticle, createTag, profile } = usePubkyClientContext();
   const [isDragging, setIsDragging] = useState(false);
-  const [isMobile, setIsMobile] = useState(false);
+  const isMobile = useIsMobile();
   const [isError, setIsError] = useState(false);
   const { addAlert } = useAlertContext();
   const [errorFile, setErrorFile] = useState('');
@@ -39,7 +33,6 @@ export default function CreateArticle({
   const [isValidContent, setIsValidContent] = useState(false);
   const wrapperRefEmojis = useRef<HTMLDivElement>(null);
   const wrapperRef = useRef<HTMLDivElement>(null);
-  const modalArticleRef = useRef<HTMLDivElement>(null);
   const [showEmojis, setShowEmojis] = useState(false);
   const [searchedUsers, setSearchedUsers] = useState<UserView[]>([]);
   const [cursorPosition, setCursorPosition] = useState<number>(0);
@@ -54,17 +47,6 @@ export default function CreateArticle({
 
   useEffect(() => {
     setPlaceholder(Utils.promptPlaceholder('article'));
-  }, []);
-
-  useEffect(() => {
-    const checkMobile = () => {
-      setIsMobile(window.innerWidth < 1280);
-    };
-
-    checkMobile();
-    window.addEventListener('resize', checkMobile);
-
-    return () => window.removeEventListener('resize', checkMobile);
   }, []);
 
   const handleSubmit = async (content: string) => {
@@ -108,23 +90,6 @@ export default function CreateArticle({
   };
 
   useEffect(() => {
-    const handleClickOutsideModals = (event: MouseEvent) => {
-      if (
-        modalArticleRef.current &&
-        !modalArticleRef.current.contains(event.target as Node)
-      ) {
-        setShowModalArticle(false);
-      }
-    };
-
-    document.addEventListener('mousedown', handleClickOutsideModals);
-
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutsideModals);
-    };
-  }, [modalArticleRef, setShowModalArticle]);
-
-  useEffect(() => {
     if (debounceTimeout) {
       clearTimeout(debounceTimeout);
     }
@@ -158,14 +123,14 @@ export default function CreateArticle({
 
   {
     /**const searchProfiles = async (text: string) => {
-    try {
-      const result = await searchUsersByUsername(text);
-      return result || [];
-    } catch (error) {
-      console.error('Error searching profiles:', error);
-      return [];
-    }
-  }; */
+        try {
+          const result = await searchUsersByUsername(text);
+          return result || [];
+        } catch (error) {
+          console.error('Error searching profiles:', error);
+          return [];
+        }
+      }; */
   }
 
   useEffect(() => {
@@ -336,194 +301,174 @@ export default function CreateArticle({
   };
 
   return (
-    <ModalUI.Root
-      modalRef={modalArticleRef}
-      show={showModalArticle}
-      closeModal={() => {
-        setShowModalArticle(false);
-        //setArrayTags([]);
-      }}
-      className="md:w-[1200px] max-h-[600px] overflow-y-auto max-w-[1200px]"
-    >
-      <ModalUI.CloseAction
-        onClick={() => {
-          setShowModalArticle(false);
-          //setArrayTags([]);
-          //setContent('');
-        }}
-      />
-      <div className="flex flex-col gap-4">
-        <ModalUI.Header title="New Article" />
-        <div className="flex items-center relative">
-          <div className="w-full">
-            <div
-              //id={`${id}`}
-              className="w-full rounded-lg flex-col justify-start items-start inline-flex"
-            >
-              <div
-                ref={wrapperRef}
-                className="w-full flex justify-between gap-3 items-start flex-col"
-              >
-                <div className="w-full">
-                  <Input.Cursor
-                    placeholder="Title"
-                    autoFocus
-                    className="h-auto text-[40px] font-bold sm:text-[64px]"
-                    defaultValue={contentTitle}
-                    disabled={sendingArticle}
-                    maxLength={30}
-                    autoCorrect="off"
-                    //error={errors.name}
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                      setContentTitle(e.target.value)
-                    }
-                  />
-                  <Section.UserArea
-                    uriPic={
-                      (profile?.image as string) ?? '/images/webp/Userpic.webp'
-                    }
-                    name={profile?.name ?? Utils.minifyPubky(pubky ?? '')}
-                    largeView={!isMobile}
-                  />
-                  <div className="relative my-4">
-                    {selectedFile.length > 0 ? (
-                      <div className="relative">
-                        <div
-                          onClick={removeFile}
-                          className="z-10 cursor-pointer absolute top-2.5 right-2.5 w-10 h-10 p-3 bg-[#05050a] bg-opacity-50 hover:bg-opacity-30 rounded-[48px] backdrop-blur-[20px] justify-center items-center inline-flex"
-                        >
-                          <Icon.Trash size="20" />
-                        </div>
-                        <Image
-                          src={URL.createObjectURL(selectedFile[0])}
-                          alt="Uploaded Preview"
-                          className="w-[1200px] h-auto max-h-[500px] rounded-lg"
-                          width={1000}
-                          height={650}
-                        />
-                      </div>
-                    ) : (
-                      <div
-                        onClick={handleFileClick}
-                        onDragEnter={handleDragEnter}
-                        onDragOver={handleDragOver}
-                        onDragLeave={handleDragLeave}
-                        onDrop={handleDrop}
-                        className="flex flex-col justify-center items-center h-[216px] min-w-[200px] bg-white bg-opacity-10 rounded-lg p-4 cursor-pointer"
-                      >
-                        <input
-                          ref={fileInputRef}
-                          type="file"
-                          accept="image/*"
-                          onChange={handleFileChange}
-                          className="hidden"
-                        />
-                        <div
-                          className={`${
-                            isDragging && selectedFile
-                              ? 'opacity-100'
-                              : 'opacity-50'
-                          } hover:opacity-100`}
-                        >
-                          <Icon.Plus size="64" />
-                          <p className="mt-2 text-white text-sm text-center">
-                            Add image
-                          </p>
-                        </div>
-                        <Typography.Body
-                          className="text-red-500 mt-4"
-                          variant="small"
-                        >
-                          {errorFile}
-                        </Typography.Body>
-                      </div>
-                    )}
+    <div className="flex items-center relative">
+      <div className="w-full">
+        <div
+          //id={`${id}`}
+          className="w-full rounded-lg flex-col justify-start items-start inline-flex"
+        >
+          <div
+            ref={wrapperRef}
+            className="w-full flex justify-between gap-3 items-start flex-col"
+          >
+            <div className="w-full">
+              <Input.Cursor
+                placeholder="Title"
+                autoFocus
+                className="h-auto text-[40px] font-bold sm:text-[64px]"
+                defaultValue={contentTitle}
+                disabled={sendingArticle}
+                maxLength={30}
+                autoCorrect="off"
+                //error={errors.name}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setContentTitle(e.target.value)
+                }
+              />
+              <Section.UserArea
+                uriPic={
+                  (profile?.image as string) ?? '/images/webp/Userpic.webp'
+                }
+                name={profile?.name ?? Utils.minifyPubky(pubky ?? '')}
+                largeView={!isMobile}
+              />
+              <div className="relative my-4">
+                {selectedFile.length > 0 ? (
+                  <div className="relative">
+                    <div
+                      onClick={removeFile}
+                      className="z-10 cursor-pointer absolute top-2.5 right-2.5 w-10 h-10 p-3 bg-[#05050a] bg-opacity-50 hover:bg-opacity-30 rounded-[48px] backdrop-blur-[20px] justify-center items-center inline-flex"
+                    >
+                      <Icon.Trash size="20" />
+                    </div>
+                    <Image
+                      src={URL.createObjectURL(selectedFile[0])}
+                      alt="Uploaded Preview"
+                      className="w-[1200px] h-auto max-h-[500px] rounded-lg"
+                      width={1000}
+                      height={650}
+                    />
                   </div>
-                  <Section.InputArea
-                    //selectedFiles={selectedFiles}
-                    //setSelectedFiles={setSelectedFiles}
-                    content={contentArticle}
-                    className="mt-[6px]"
-                    setContent={setContentArticle}
-                    searchedUsers={searchedUsers}
-                    setSearchedUsers={setSearchedUsers}
-                    setCursorPosition={setCursorPosition}
-                    maxLength={50000}
-                    isError={isError}
-                    setIsError={setIsError}
-                    //setTextArea={setTextArea}
-                    largeView={!isMobile}
-                    setIsValidContent={setIsValidContent}
-                    placeHolder={placeholder}
-                    //setFilePreviews={setFilePreviews}
-                    loading={sendingArticle}
-                    markdown
-                  />
-                </div>
-                <LinkPreviewer content={contentArticle} />
-                <Section.FooterArea
-                  loading={sendingArticle}
-                  visibleTextArea
-                  textArea
-                  content={contentArticle}
-                  setContent={setContentArticle}
-                  cursorPosition={cursorPosition}
-                  setCursorPosition={setCursorPosition}
-                  setIsValidContent={setIsValidContent}
-                  //setSelectedFiles={setSelectedFiles}
-                  //selectedFiles={selectedFiles}
-                  setArrayTags={setArrayTags}
-                  maxLength={50000}
-                  arrayTags={arrayTags}
-                  //setFilePreviews={setFilePreviews}
-                  showEmojis={showEmojis}
-                  setShowEmojis={setShowEmojis}
-                  largeView={!isMobile}
-                  noFile
-                  button={
-                    <Button.Medium
-                      id="post-btn"
-                      className="w-auto"
-                      variant="line"
-                      icon={
-                        <Icon.PaperPlaneRight
-                          color={
-                            !isValidContent ||
-                            selectedFile.length === 0 ||
-                            isError ||
-                            !contentTitle
-                              ? 'gray'
-                              : 'white'
-                          }
-                        />
-                      }
-                      disabled={
+                ) : (
+                  <div
+                    onClick={handleFileClick}
+                    onDragEnter={handleDragEnter}
+                    onDragOver={handleDragOver}
+                    onDragLeave={handleDragLeave}
+                    onDrop={handleDrop}
+                    className="flex flex-col justify-center items-center h-[216px] min-w-[200px] bg-white bg-opacity-10 rounded-lg p-4 cursor-pointer"
+                  >
+                    <input
+                      ref={fileInputRef}
+                      type="file"
+                      accept="image/*"
+                      onChange={handleFileChange}
+                      className="hidden"
+                    />
+                    <div
+                      className={`${
+                        isDragging && selectedFile
+                          ? 'opacity-100'
+                          : 'opacity-50'
+                      } hover:opacity-100`}
+                    >
+                      <Icon.Plus size="64" />
+                      <p className="mt-2 text-white text-sm text-center">
+                        Add image
+                      </p>
+                    </div>
+                    <Typography.Body
+                      className="text-red-500 mt-4"
+                      variant="small"
+                    >
+                      {errorFile}
+                    </Typography.Body>
+                  </div>
+                )}
+              </div>
+              <Section.InputArea
+                //selectedFiles={selectedFiles}
+                //setSelectedFiles={setSelectedFiles}
+                content={contentArticle}
+                className="mt-[6px]"
+                setContent={setContentArticle}
+                searchedUsers={searchedUsers}
+                setSearchedUsers={setSearchedUsers}
+                setCursorPosition={setCursorPosition}
+                maxLength={50000}
+                isError={isError}
+                setIsError={setIsError}
+                //setTextArea={setTextArea}
+                largeView={!isMobile}
+                setIsValidContent={setIsValidContent}
+                placeHolder={placeholder}
+                //setFilePreviews={setFilePreviews}
+                loading={sendingArticle}
+                markdown
+              />
+            </div>
+            <LinkPreviewer content={contentArticle} />
+            <Section.FooterArea
+              loading={sendingArticle}
+              visibleTextArea
+              textArea
+              content={contentArticle}
+              setContent={setContentArticle}
+              cursorPosition={cursorPosition}
+              setCursorPosition={setCursorPosition}
+              setIsValidContent={setIsValidContent}
+              //setSelectedFiles={setSelectedFiles}
+              //selectedFiles={selectedFiles}
+              setArrayTags={setArrayTags}
+              maxLength={50000}
+              arrayTags={arrayTags}
+              //setFilePreviews={setFilePreviews}
+              showEmojis={showEmojis}
+              setShowEmojis={setShowEmojis}
+              largeView={!isMobile}
+              noFile
+              button={
+                <Button.Medium
+                  id="post-btn"
+                  className="w-auto"
+                  variant="line"
+                  icon={
+                    <Icon.PaperPlaneRight
+                      color={
                         !isValidContent ||
                         selectedFile.length === 0 ||
                         isError ||
                         !contentTitle
+                          ? 'gray'
+                          : 'white'
                       }
-                      loading={sendingArticle}
-                      onClick={
-                        isValidContent &&
-                        selectedFile.length > 0 &&
-                        contentTitle &&
-                        !isError &&
-                        !sendingArticle
-                          ? () => handleSubmit(contentArticle)
-                          : undefined
-                      }
-                    >
-                      Publish
-                    </Button.Medium>
+                    />
                   }
-                  wrapperRefEmojis={wrapperRefEmojis}
-                />
-              </div>
-            </div>
+                  disabled={
+                    !isValidContent ||
+                    selectedFile.length === 0 ||
+                    isError ||
+                    !contentTitle
+                  }
+                  loading={sendingArticle}
+                  onClick={
+                    isValidContent &&
+                    selectedFile.length > 0 &&
+                    contentTitle &&
+                    !isError &&
+                    !sendingArticle
+                      ? () => handleSubmit(contentArticle)
+                      : undefined
+                  }
+                >
+                  Publish
+                </Button.Medium>
+              }
+              wrapperRefEmojis={wrapperRefEmojis}
+            />
           </div>
         </div>
       </div>
-    </ModalUI.Root>
+    </div>
   );
 }
