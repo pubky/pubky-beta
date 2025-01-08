af8ca05489105af165aa8e17535d2a1de8b77d68
Miguel Medeiros <miguel@miguelmedeiros.com.br>
2025-01-08 19:07:59 -0300

    feat: tooltip checkmark component transition animations

--- apps/web/components/Tooltip/_CheckMark.tsx
+++ apps/web/components/Tooltip/_CheckMark.tsx
@@ -27,19 +27,27 @@ export default function TooltipCheckMark({
     >
       {children}
 
-      {isVisible && (
+      <div
+        className={clsx(
+          'absolute z-50 bottom-full left-1/2 -translate-x-1/2 mb-2',
+          'transition-all duration-200 ease-in-out',
+          'transform',
+          isVisible
+            ? 'opacity-100 translate-y-0'
+            : 'opacity-0 translate-y-2 pointer-events-none',
+        )}
+      >
         <Tooltip.Main
           id="tooltip-checkmark"
           className={clsx(
-            'absolute z-50 bottom-full left-1/2 -translate-x-1/2 mb-2',
             'w-max px-2 py-1 text-sm whitespace-nowrap',
             'bg-neutral-900 text-neutral-200 rounded-md shadow-md cursor-default',
-            'transition-opacity duration-200 ease-out opacity-100',
+            'before:border-t-neutral-900',
           )}
         >
           {content}
         </Tooltip.Main>
-      )}
+      </div>
     </div>
   );
 }
