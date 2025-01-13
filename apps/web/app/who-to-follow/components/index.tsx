753cdfff82d3509e0c3a792efbef532a44d15a05
flaviomoceri <moceri.flavio@gmail.com>
2025-01-13 19:01:30 +0100

    fix infiniteScroll profile + whoToFollow

--- apps/web/app/who-to-follow/components/index.tsx
+++ apps/web/app/who-to-follow/components/index.tsx
@@ -1,19 +1,11 @@
-import { Buttons } from './_Buttons';
 import { LeftSidebar } from './_LeftSidebar';
-import { Counters } from './_Counters';
 import { Header } from './_Header';
 import MainContent from './_MainContent';
 import { RightSidebar } from './_RightSidebar';
-import { Tags } from './_Tags';
-import { User } from './_User';
 
 export const RecommendedUsers = {
-  Buttons,
-  Counters,
   Header,
-  Tags,
   LeftSidebar,
   MainContent,
   RightSidebar,
-  User,
 };
