7861c5732fece053f5b8719043c99ddb310a6077
flaviomoceri <moceri.flavio@gmail.com>
2025-01-22 11:49:24 +0100

    add faq questions

--- apps/web/components/DropDown/_Question.tsx
+++ apps/web/components/DropDown/_Question.tsx
@@ -38,12 +38,7 @@ export default function Question({
           open ? 'max-h-screen opacity-100' : 'max-h-0 opacity-0 -mt-6'
         )}
       >
-        <Typography.Body
-          className="text-opacity-80 text-[16px] mr-2 leading-snug"
-          variant="medium"
-        >
-          {answer}
-        </Typography.Body>
+        {answer}
       </div>
     </div>
   );
