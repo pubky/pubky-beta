7cc2450f19f8f31dfe4cf870c86ede8b44bf5bc6
Flavio <moceri.flavio@gmail.com>
2025-01-24 18:35:16 +0100

    add code syntax (#909)

--- apps/web/app/settings/sections/_privacy.tsx
+++ apps/web/app/settings/sections/_privacy.tsx
@@ -27,7 +27,7 @@ export default function PrivacySafety() {
         <Typography.Body variant="medium" className="text-opacity-80">
           Privacy is not a crime. Manage your visibility and safety on Pubky.
         </Typography.Body>
-        <div className="w-full p-6 bg-white bg-opacity-5 rounded-2xl flex-col justify-start items-start gap-6 inline-flex">
+        <div className="w-full p-6 bg-white bg-opacity-5 shadow-[0px_20px_40px_0px_rgba(5,5,10,0.50)] rounded-2xl flex-col justify-start items-start gap-6 inline-flex">
           <div className="w-full h-8 justify-between items-center inline-flex">
             <Typography.Body variant="small-bold">
               Show CheckLink before redirecting
