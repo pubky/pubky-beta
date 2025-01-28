7c867dc816c0e114848e96b092ae5194b1d7999b
SHAcollision <cotyleron@proton.me>
2025-01-28 18:26:56 -0400

    Remove unecesary setSpecs on pk change

--- apps/web/contexts/_pubky.tsx
+++ apps/web/contexts/_pubky.tsx
@@ -338,8 +338,6 @@ export function PubkyClientWrapper({
       const pk = publickey;
 
       setPubkyAndStorage(pk);
-      const specsBuilder = new PubkySpecsBuilder(pk);
-      setSpecsBuilder(specsBuilder);
       return pk;
     } catch (error: any) {
       // Get error message and return as a string
@@ -370,8 +368,6 @@ export function PubkyClientWrapper({
       const pk = session.pubky().z32();
 
       setPubkyAndStorage(pk);
-      const specsBuilder = new PubkySpecsBuilder(pk);
-      setSpecsBuilder(specsBuilder);
       return pk;
     } catch (error: any) {
       // Get error message and return as a string
@@ -403,8 +399,6 @@ export function PubkyClientWrapper({
       const pk = session.pubky().z32();
 
       setPubkyAndStorage(pk);
-      const specsBuilder = new PubkySpecsBuilder(pk);
-      setSpecsBuilder(specsBuilder);
       return pk;
     } catch (error: any) {
       // Get error message and return as a string
