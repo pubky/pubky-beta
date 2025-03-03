ba887de3fc2760f8a00e1a3348980a1cc7e967c2
Miguel Medeiros <miguel@miguelmedeiros.com.br>
2025-03-03 17:29:59 -0300

    update TypeScript and Tailwind configuration files for improved path resolution

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const { join } = require('path');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [join(__dirname, '{pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}')],
+  content: [join(__dirname, 'src/{pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}')],
   theme: {
     extend: {
       fontFamily: {
