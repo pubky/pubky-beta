e003e5bcc65c1192cfbd485cbae6d137c116125a
flaviomoceri <moceri.flavio@gmail.com>
2025-03-05 11:02:03 +0100

    fix tag gap in largeView

--- src/components/Post/Tags/_LargeView.tsx
+++ src/components/Post/Tags/_LargeView.tsx
@@ -9,7 +9,7 @@ interface TagsLargeViewProps extends React.HTMLAttributes<HTMLDivElement> {
 export default function LargeView({ post }: TagsLargeViewProps) {
   return (
     <div className="mt-1.5 w-auto cursor-default" onClick={(event) => event.stopPropagation()}>
-      <div className={`min-w-[380px] flex-col inline-flex gap-2`}>
+      <div className="min-w-[380px] flex-col inline-flex gap-0.5">
         <TagsLargeView post={post} />
         <div className="flex">
           <InputTag post={post} />
