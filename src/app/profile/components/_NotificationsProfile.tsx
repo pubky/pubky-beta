20df5b262825a82e9267afb98d6a5e244f31cba3
flaviomoceri <moceri.flavio@gmail.com>
2025-03-14 20:52:56 +0100

    remove filter notifications logic

--- src/app/profile/components/_NotificationsProfile.tsx
+++ src/app/profile/components/_NotificationsProfile.tsx
@@ -6,27 +6,17 @@ import { useFilterContext, useNotificationsContext, usePubkyClientContext } from
 import { Icon } from '@social/ui-shared';
 import { useInfiniteScroll } from '@/hooks/useInfiniteScroll';
 import { useEffect, useState } from 'react';
-import { filterMap } from '@/contexts/_notifications';
 import Image from 'next/image';
 
 export default function NotificationsProfile() {
-  const {
-    notifications,
-    loading: loadingNotifications,
-    loadMoreNotifications,
-    selectedFilter
-  } = useNotificationsContext();
+  const { notifications, loading: loadingNotifications, loadMoreNotifications } = useNotificationsContext();
   const { unReadNotification, setUnReadNotification } = useFilterContext();
   const [tempUnReadNotification, setTempUnReadNotification] = useState(0);
   const { putTimestampNotification } = usePubkyClientContext();
 
   const loader = useInfiniteScroll(loadMoreNotifications, loadingNotifications);
 
-  const filteredNotifications = notifications.filter(
-    (notification) => selectedFilter === 'all' || filterMap[selectedFilter]?.includes(notification.body.type)
-  );
-
-  const displayedNotifications = filteredNotifications.slice(tempUnReadNotification);
+  const displayedNotifications = notifications.slice(tempUnReadNotification);
 
   useEffect(() => {
     if (unReadNotification) {
@@ -55,32 +45,23 @@ export default function NotificationsProfile() {
           </div>
         </ContentNotFound>
       ) : (
-        <div className="flex flex-col gap-1">
-          <div id="notifications-list" className="px-6 py-[18px] bg-white/10 rounded-lg">
-            {tempUnReadNotification > 0 && (
-              <div>
-                {notifications.slice(0, tempUnReadNotification).map((notification, index) => (
-                  <Notifications.Notification key={index} notification={notification} unread />
-                ))}
+        <div className="px-6 py-[18px] bg-white/10 rounded-lg">
+          {tempUnReadNotification > 0 && (
+            <div>
+              {notifications.slice(0, tempUnReadNotification).map((notification, index) => (
+                <Notifications.Notification key={index} notification={notification} unread />
+              ))}
+            </div>
+          )}
+          {displayedNotifications.map((notification, index) => {
+            const isLastElement = index === displayedNotifications.length - 1;
+
+            return (
+              <div key={index} ref={isLastElement ? loader : null}>
+                <Notifications.Notification notification={notification} />
               </div>
-            )}
-            {displayedNotifications.length > 0 ? (
-              displayedNotifications.map((notification, index) => {
-                const isLastElement = index === displayedNotifications.length - 1;
-                return (
-                  <div key={index} ref={isLastElement ? loader : null}>
-                    <Notifications.Notification notification={notification} />
-                  </div>
-                );
-              })
-            ) : (
-              <ContentNotFound
-                icon={<Icon.SmileySad size="48" color="#C8FF00" />}
-                title="No notifications to show"
-                description="Try selecting a different filter"
-              />
-            )}
-          </div>
+            );
+          })}
         </div>
       )}
       {loadingNotifications && (
