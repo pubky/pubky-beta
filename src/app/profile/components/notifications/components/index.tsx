ddf62fcb9c73b157de6e942684400eee1af74cfb
flaviomoceri <moceri.flavio@gmail.com>
2025-03-19 20:04:19 +0100

    removed group notifications

--- src/app/profile/components/notifications/components/index.tsx
+++ src/app/profile/components/notifications/components/index.tsx
@@ -1,15 +1,9 @@
 import Notification from './_Notification';
-import NotificationGroup from './_NotificationGroup';
-import NotificationTagGroup from './_NotificationTagGroup';
-import NotificationTagPostGroup from './_NotificationTagPostGroup';
 import Root from './_Root';
 import Content from './_Content';
 
 export const Notifications = {
   Notification,
-  NotificationGroup,
-  NotificationTagGroup,
-  NotificationTagPostGroup,
   Root,
   Content
 };
