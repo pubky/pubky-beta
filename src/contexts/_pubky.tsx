71fabe16aee02d0b40e4df25423f22cfc0e468c1
tipogi <tipogi@pm.me>
2025-03-21 17:38:39 +0100

    prettier fix

--- src/contexts/_pubky.tsx
+++ src/contexts/_pubky.tsx
@@ -270,7 +270,7 @@ export function PubkyClientWrapper({ children }: { children: React.ReactNode }) 
     // TODO: Temporary fix — this should eventually be replaced with middleware
     // More info in https://github.com/pubky/pubky-app/pull/1235
     if (!pubky && !Utils.storage.get('pubky_public_key')) {
-      console.log('Removing pubky context and localStorage...')
+      console.log('Removing pubky context and localStorage...');
       Utils.storage.remove('pubky_public_key');
       setPubky(undefined);
       return false;
