701a8aee31eac0a6277f11f99c68e09af31f3d41
flaviomoceri <moceri.flavio@gmail.com>
2025-03-26 12:15:44 +0100

    add protecton createRepost

--- src/components/Modal/_CreateRepost/index.tsx
+++ src/components/Modal/_CreateRepost/index.tsx
@@ -1,6 +1,10 @@
+'use client';
+
+import { useState } from 'react';
 import { Modal } from '@social/ui-shared';
 import { PostView } from '@/types/Post';
 import ContentCreateRepost from './_Content';
+import { useModal } from '@/contexts';
 
 interface CreateRepostProps {
   showModal: boolean;
@@ -9,17 +13,28 @@ interface CreateRepostProps {
 }
 
 export default function CreateRepost({ showModal, setShowModal, post }: CreateRepostProps) {
+  const { openModal } = useModal();
+  const [hasContent, setHasContent] = useState(false);
+
+  const handleClose = () => {
+    if (hasContent) {
+      openModal('checkContent', { setShow2: setShowModal });
+    } else {
+      setShowModal(false);
+    }
+  };
+
   return (
     <Modal.Root
       show={showModal}
-      closeModal={() => setShowModal(false)}
+      closeModal={handleClose}
       className="md:w-[792px] max-w-[1200px] max-h-[90vh] overflow-y-auto scrollbar-thin scrollbar-webkit"
     >
-      <Modal.CloseAction onClick={() => setShowModal(false)} />
+      <Modal.CloseAction onClick={handleClose} />
       <div className="mb-6">
         <Modal.Header title="Repost" />
       </div>
-      <ContentCreateRepost setShowModalRepost={setShowModal} post={post} />
+      <ContentCreateRepost setShowModalRepost={setShowModal} post={post} setHasContent={setHasContent} />
     </Modal.Root>
   );
 }
