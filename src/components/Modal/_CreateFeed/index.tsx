27b1622ad52c2aebfe1c3874c1c6b8b64701bbd3
flaviomoceri <moceri.flavio@gmail.com>
2025-03-26 14:56:46 +0100

    fix eslint

--- src/components/Modal/_CreateFeed/index.tsx
+++ src/components/Modal/_CreateFeed/index.tsx
@@ -15,7 +15,9 @@ export default function CreateFeed({ showModal, setShowModal, handleLoadFeeds }:
       className="md:w-[620px] max-h-[90vh] overflow-y-auto justify-start scrollbar-thin scrollbar-webkit"
     >
       <Modal.CloseAction onClick={() => setShowModal(false)} />
-      <div className='mb-4'><Modal.Header title="Create Feed" /></div>
+      <div className="mb-4">
+        <Modal.Header title="Create Feed" />
+      </div>
       <ContentCreateFeed handleLoadFeeds={handleLoadFeeds} setShowModalCreateFeed={setShowModal} />
     </Modal.Root>
   );
