7216bc492430fcf7ec9ef43206c0ebb3aa0e24b0
Miguel Medeiros <miguel@miguelmedeiros.com.br>
2025-03-26 15:57:16 -0300

    refactor: enhance Replies component by improving state management, integrating post fetching logic, and adding filtering for deleted posts

--- src/app/profile/components/_Replies.tsx
+++ src/app/profile/components/_Replies.tsx
@@ -1,77 +1,96 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { Icon } from '@social/ui-shared';
 import { ContentNotFound, Post, Skeleton } from '@/components';
-import { usePubkyClientContext } from '@/contexts';
+import { useModal, usePubkyClientContext } from '@/contexts';
 import { useInfiniteScroll } from '@/hooks/useInfiniteScroll';
 import { PostView } from '@/types/Post';
-import { useStreamPost } from '@/hooks/useStream';
 import { Profile } from '.';
 import Image from 'next/image';
+import { getStreamPosts } from '@/services/streamService';
 
 export default function Index({ creatorPubky }: { creatorPubky?: string }) {
   const limit = 10;
-  const { pubky } = usePubkyClientContext();
+  const { pubky, setTimeline, timeline, deletedPosts } = usePubkyClientContext();
   const isMyProfile = !!(pubky === creatorPubky || !creatorPubky);
-  const [timeline, setTimeline] = useState<PostView[]>([]);
-  const [start, setStart] = useState<number | undefined>(undefined);
-  const [fetching, setFetching] = useState<boolean>(false);
+  const [skip, setSkip] = useState<number>(0);
+  const [isLoading, setIsLoading] = useState(true);
+  const [finishedLoading, setFinishedLoading] = useState(false);
+  const currentPubky = creatorPubky ?? pubky ?? '';
+
+  const fetchPosts = async ({
+    skipValue = skip,
+    timelineValue = timeline
+  }: {
+    skipValue?: number;
+    timelineValue?: PostView[];
+  }) => {
+    setIsLoading(true);
+
+    try {
+      const data = await getStreamPosts(
+        currentPubky, // viewerId
+        'author_replies', // source
+        currentPubky, // authorId
+        limit, // limit
+        undefined, // start
+        undefined, // end
+        skipValue, // skip
+        'recent', // sort
+        undefined, // tags
+        undefined // kind
+      );
+
+      setSkip(skipValue + limit);
+
+      // filter out deleted posts
+      const filteredData = data.filter((post) => !deletedPosts.includes(post.details.id));
+      setTimeline([...timelineValue, ...filteredData]);
+    } catch (error) {
+      setFinishedLoading(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const initializeTimeline = async () => {
+    setSkip(0);
+    setFinishedLoading(false);
+    setTimeline([]);
+
+    await fetchPosts({
+      skipValue: 0,
+      timelineValue: []
+    });
+  };
+
+  useEffect(() => {
+    setTimeline([]);
+    setSkip(0);
+    setFinishedLoading(false);
+
+    fetchPosts({
+      skipValue: 0,
+      timelineValue: []
+    });
+  }, [currentPubky]);
+
+  const initializeTimelineCallback = useCallback(() => {
+    initializeTimeline();
+  }, [creatorPubky]);
+
+  useEffect(() => {
+    initializeTimelineCallback();
+  }, [creatorPubky]);
+
+  const loader = useInfiniteScroll(() => fetchPosts({ skipValue: skip, timelineValue: timeline }), isLoading);
+
   const lineHorizontalCSS = (
     <div className="absolute ml-[9px] -mt-2">
       <Icon.LineHorizontal size="14" color="#444447" />
     </div>
   );
-
-  const currentPubky = creatorPubky ?? pubky ?? '';
-
-  const { data, isLoading } = useStreamPost(
-    currentPubky,
-    'author_replies',
-    currentPubky,
-    limit,
-    start,
-    undefined,
-    undefined,
-    'recent'
-  );
-
-  const fetchPosts = async () => {
-    if (fetching || !data) return;
-    setFetching(true);
-
-    try {
-      if (!Array.isArray(data) || data.length === 0) {
-        setTimeline([]);
-
-        return;
-      }
-
-      const lastPost = data[data.length - 1] as PostView;
-
-      setTimeline((prev) => {
-        const newPosts = data.filter((post) => !prev.some((p) => p.details.id === post.details.id));
-        return [...prev, ...newPosts];
-      });
-
-      if (lastPost?.details?.indexed_at) {
-        setStart(lastPost.details.indexed_at - 1);
-      }
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setFetching(false);
-    }
-  };
-
-  useEffect(() => {
-    setStart(undefined);
-    setTimeline([]);
-    setFetching(false);
-    fetchPosts();
-  }, [currentPubky]);
-
-  const loader = useInfiniteScroll(fetchPosts, isLoading);
 
   return (
     <div className="flex flex-col gap-3">
@@ -87,13 +106,13 @@ export default function Index({ creatorPubky }: { creatorPubky?: string }) {
           </div>
         </div>
       ))}
-      {(isLoading || fetching) && (
+      {isLoading && !finishedLoading && (
         <div className="flex flex-col gap-3">
           <Skeleton.Simple />
         </div>
       )}
-      <div ref={loader} />
-      {!isLoading && !fetching && timeline.length === 0 && (
+      {!isLoading && !finishedLoading && <div ref={loader} />}
+      {finishedLoading && timeline.length === 0 && (
         <ContentNotFound
           icon={<Icon.NoteBlank size="48" color="#C8FF00" />}
           title="No replies yet"
