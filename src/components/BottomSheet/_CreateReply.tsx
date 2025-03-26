9d6d47e14ac38a4357e44a994ec9fefb98e49766
flaviomoceri <moceri.flavio@gmail.com>
2025-03-26 12:11:59 +0100

    add protecton createReply

--- src/components/BottomSheet/_CreateReply.tsx
+++ src/components/BottomSheet/_CreateReply.tsx
@@ -1,8 +1,10 @@
 'use client';
 
+import { useState } from 'react';
 import { BottomSheet } from '@social/ui-shared';
 import { PostView } from '@/types/Post';
 import ContentCreateReply from '../Modal/_CreateReply/_Content';
+import { useModal } from '@/contexts';
 
 interface CreateReplyProps {
   show: boolean;
@@ -13,9 +15,25 @@ interface CreateReplyProps {
 }
 
 export default function CreateReply({ show, setShow, post, title, className }: CreateReplyProps) {
+  const { openModal } = useModal();
+  const [hasContent, setHasContent] = useState(false);
+
+  const handleClose = () => {
+    if (hasContent) {
+      openModal('checkContent', { setShow2: setShow });
+    } else {
+      setShow(false);
+    }
+  };
+
   return (
-    <BottomSheet.Root show={show} setShow={setShow} title={title ?? 'Reply'} className={className}>
-      <ContentCreateReply className="p-0 border-none" setShowModalReply={setShow} post={post} />
+    <BottomSheet.Root show={show} setShow={handleClose} title={title ?? 'Reply'} className={className}>
+      <ContentCreateReply
+        className="p-0 border-none"
+        setShowModalReply={setShow}
+        post={post}
+        setHasContent={setHasContent}
+      />
     </BottomSheet.Root>
   );
 }
--- src/components/Modal/_CreateReply/index.tsx
+++ src/components/Modal/_CreateReply/index.tsx
@@ -1,6 +1,10 @@
+'use client';
+
+import { useState } from 'react';
 import { Modal } from '@social/ui-shared';
 import { PostView } from '@/types/Post';
 import ContentCreateReply from './_Content';
+import { useModal } from '@/contexts';
 
 interface CreateReplyProps {
   showModal: boolean;
@@ -9,17 +13,28 @@ interface CreateReplyProps {
 }
 
 export default function CreateReply({ showModal, setShowModal, post }: CreateReplyProps) {
+  const { openModal } = useModal();
+  const [hasContent, setHasContent] = useState(false);
+
+  const handleClose = () => {
+    if (hasContent) {
+      openModal('checkContent', { setShow2: setShowModal });
+    } else {
+      setShowModal(false);
+    }
+  };
+
   return (
     <Modal.Root
       show={showModal}
-      closeModal={() => setShowModal(false)}
+      closeModal={handleClose}
       className="md:w-[792px] max-w-[1200px] max-h-[90vh] overflow-y-auto scrollbar-thin scrollbar-webkit"
     >
-      <Modal.CloseAction onClick={() => setShowModal(false)} />
+      <Modal.CloseAction onClick={handleClose} />
       <div className="mb-4">
         <Modal.Header title="Reply" />
       </div>
-      <ContentCreateReply setShowModalReply={setShowModal} post={post} />
+      <ContentCreateReply setShowModalReply={setShowModal} post={post} setHasContent={setHasContent} />
     </Modal.Root>
   );
 }
