a7199491c48bfd23be3994363c80a5050efd453e
Flavio <moceri.flavio@gmail.com>
2025-03-28 13:18:58 +0100

    feat: add pubkyRings links stores (#1314)

--- src/components/ui-shared/lib/Content/_LinksStoreApp.tsx
+++ src/components/ui-shared/lib/Content/_LinksStoreApp.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { twMerge } from 'tailwind-merge';
 
 interface LinksStoreAppProps extends React.HTMLAttributes<HTMLDivElement> {
@@ -8,8 +9,20 @@ export const LinksStoreApp = ({ ...rest }: LinksStoreAppProps) => {
   const baseCSS = 'relative sm:-ml-2 mt-4 self-stretch items-center justify-center sm:justify-start flex';
   return (
     <div {...rest} className={twMerge(baseCSS, rest.className)}>
-      <img className="w-full h-[65px] sm:w-[112px] sm:h-[45px]" src="/images/webp/apple-store.webp" alt="Apple Store" />
-      <img className="w-full h-[65px] sm:w-[112px] sm:h-[45px]" src="/images/webp/google-play.webp" alt="Google Play" />
+      <Link target="_blank" href="https://testflight.apple.com/join/kwK4kf7a">
+        <img
+          className="w-full h-[65px] sm:w-[112px] sm:h-[45px]"
+          src="/images/webp/apple-store.webp"
+          alt="Apple Store"
+        />
+      </Link>
+      <Link target="_blank" href="https://play.google.com/apps/testing/to.pubkyring">
+        <img
+          className="w-full h-[65px] sm:w-[112px] sm:h-[45px]"
+          src="/images/webp/google-play.webp"
+          alt="Google Play"
+        />
+      </Link>
     </div>
   );
 };
