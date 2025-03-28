af8b66012eddb808d201620b26647de8c7bcd4d9
flaviomoceri <moceri.flavio@gmail.com>
2025-03-28 11:49:30 +0100

    fix metadata in profile

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,10 +1,11 @@
+import { getSeoMetadata } from '@/components/HeaderSEO';
 import { Profile } from './components/index';
 import LayoutProfile from './components/_LayoutProfile';
 
-export const metadata = {
+export const metadata = getSeoMetadata({
   title: 'Profile | Pubky.app',
   description: 'Pubky.app - Unlock the web.'
-};
+});
 
 export default function Index() {
   return (
