4dfc25a3bbfc82e929f229220699d0689e69ac87
Miguel Medeiros <miguel@miguelmedeiros.com.br>
2025-03-31 17:41:26 -0300

    refactor: simplify content extraction in Post component

--- src/components/Post/_Content.tsx
+++ src/components/Post/_Content.tsx
@@ -49,13 +49,7 @@ export default function Content({
     return text.replace(/\n{3,}/g, '\n\n');
   };
 
-  const stripHtml = (html: string) => {
-    return html.replace(/<[^>]*>/g, '');
-  };
-
-  // check long post kind
-  const isLongPost = String(post?.details?.kind) === PubkyAppPostKind[1].toLocaleLowerCase();
-  const text = isLongPost ? stripHtml(JSON.parse(post?.details?.content).body) : stripHtml(post?.details?.content);
+  const text = post?.details?.content;
   const files = post?.details?.attachments;
   const uri = post?.details?.uri;
   const generateFileUrl = (file: FileView, type = 'main') => `${BASE_URL}/${file.owner_id}/${file.id}/${type}`;
