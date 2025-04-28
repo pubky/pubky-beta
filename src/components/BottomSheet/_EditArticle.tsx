a1140214c3501e6335d2d6810f8fa034ec025ea3
Flavio Moceri <moceri.flavio@gmail.com>
2025-04-28 10:48:13 +0200

    fix build

--- src/components/BottomSheet/_EditArticle.tsx
+++ src/components/BottomSheet/_EditArticle.tsx
@@ -15,13 +15,8 @@ interface EditArticleProps {
 
 export default function EditArticle({ show, setShow, article, setShowMenu, title, className }: EditArticleProps) {
   return (
-    <BottomSheet.Root show={show} setShow={setShow} title={title} className={className}>
-      <ContentEditArticle
-        className="p-0 border-none"
-        setShowModalEditArticle={setShow}
-        article={article}
-        setShowMenu={setShowMenu}
-      />
+    <BottomSheet.Root show={show} setShow={setShow} title={title ?? 'Edit Article'} className={className}>
+      <ContentEditArticle setShowModalEditArticle={setShow} article={article} setShowMenu={setShowMenu} />
     </BottomSheet.Root>
   );
 }
