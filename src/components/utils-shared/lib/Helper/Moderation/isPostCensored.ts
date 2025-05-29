99e202f1f252d892e867ef5c9c43b8a45d080c34
Flavio Moceri <moceri.flavio@gmail.com>
2025-05-29 15:28:43 +0200

    fix build

--- src/components/utils-shared/lib/Helper/Moderation/isPostCensored.ts
+++ src/components/utils-shared/lib/Helper/Moderation/isPostCensored.ts
@@ -1,7 +1,9 @@
 import { Utils } from '@/components/utils-shared';
 import { PostView } from '@/types/Post';
 
-const censoredTags = JSON.parse(process.env.NEXT_PUBLIC_MODERATED_TAGS) || Utils.censoredTags;
+const censoredTags = process.env.NEXT_PUBLIC_MODERATED_TAGS 
+  ? JSON.parse(process.env.NEXT_PUBLIC_MODERATED_TAGS)
+  : Utils.censoredTags;
 
 const isCensored = (postToCheck: PostView) => {
   return postToCheck?.tags.some(
--- src/components/utils-shared/lib/Helper/Moderation/isTagCensored.ts
+++ src/components/utils-shared/lib/Helper/Moderation/isTagCensored.ts
@@ -1,7 +1,9 @@
 import { Utils } from '@/components/utils-shared';
 import { PostTag } from '@/types/Post';
 
-const censoredTags = JSON.parse(process.env.NEXT_PUBLIC_MODERATED_TAGS) || Utils.censoredTags;
+const censoredTags = process.env.NEXT_PUBLIC_MODERATED_TAGS
+  ? JSON.parse(process.env.NEXT_PUBLIC_MODERATED_TAGS)
+  : Utils.censoredTags;
 
 const isTagCensored = (tag: PostTag) => {
   return censoredTags.includes(tag.label) && tag.taggers[0] === process.env.NEXT_PUBLIC_MODERATION_ID;
