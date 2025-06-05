b088f71ad3a5c5c21704fa122f4d58a19aaf3b17
Miguel Medeiros <miguel@miguelmedeiros.com.br>
2025-06-05 09:21:49 -0300

    chore: update .env.example and fix Analytics component to use NEXT_PUBLIC_PLAUSIBLE

--- src/components/Analytics/index.ts
+++ src/components/Analytics/index.ts
@@ -1,11 +1,11 @@
 import React from 'react';
 
 export const Analytics: React.FC = () => {
-  if (!process.env.PLAUSIBLE) return null;
+  if (!process.env.NEXT_PUBLIC_PLAUSIBLE) return null;
 
   return React.createElement('script', {
     defer: true,
-    'data-domain': process.env.PLAUSIBLE,
+    'data-domain': process.env.NEXT_PUBLIC_PLAUSIBLE,
     src: 'https://synonym.to/js/script.js'
   });
 };
