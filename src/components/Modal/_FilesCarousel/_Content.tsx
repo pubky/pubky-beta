86980a88ab5868a66267c72042a8a336b78d818d
Flavio Moceri <moceri.flavio@gmail.com>
2025-06-18 08:49:17 +0200

    feat: enable zoom filesCarousel

--- src/components/Modal/_FilesCarousel/_Content.tsx
+++ src/components/Modal/_FilesCarousel/_Content.tsx
@@ -78,8 +78,7 @@ export default function ContentFilesCarousel({ fileContents, currentFileIndex }:
   const currentFile = mediaFiles[localFileIndex];
   const isVideo = currentFile?.content_type.startsWith('video');
 
-  {
-    /** 
+ 
   // Enable zoom
   useEffect(() => {
     const metaTag = document.querySelector('meta[name="viewport"]') as HTMLMetaElement | null;
@@ -91,8 +90,7 @@ export default function ContentFilesCarousel({ fileContents, currentFileIndex }:
       metaTag.content = 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no';
     };
   }, [localFileIndex]);
-  */
-  }
+  
 
   return (
     <div
