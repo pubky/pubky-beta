c8e96f05202aa7a7485a103e30ddbaea129af8f8
Flavio Moceri <moceri.flavio@gmail.com>
2025-08-04 12:59:50 +0200

    fix loading

--- src/app/post/[pubky]/[postId]/PostPageClient.tsx
+++ src/app/post/[pubky]/[postId]/PostPageClient.tsx
@@ -81,5 +81,9 @@ export default function PostPage({ pubky, postId }: PostPageClientProps) {
   }, [pubky, postId, currentUserPubky, openModal]);
 
   // This component doesn't render anything visible
-  return null;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-white" />
+    </div>
+  );
 }
